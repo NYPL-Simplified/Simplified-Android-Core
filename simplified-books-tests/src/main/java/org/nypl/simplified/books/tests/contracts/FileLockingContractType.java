c325f36c3a51dea99d9ee0560af1cbea56fb5dcd
Mark Raynsford <code@io7m.com>
2015-04-01 20:20:17 +0000

    Add file locking utilities and tests

---
+++ simplified-books-tests/src/main/java/org/nypl/simplified/books/tests/contracts/FileLockingContractType.java
@@ -0,0 +1,10 @@
+package org.nypl.simplified.books.tests.contracts;
+
+public interface FileLockingContractType
+{
+  void testLocking0()
+    throws Exception;
+
+  void testLocking1()
+    throws Exception;
+}
