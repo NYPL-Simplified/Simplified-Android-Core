af6fe722023dbc3a99e613fb0bb0998b1d073050
Mark Raynsford <code@io7m.com>
2015-05-15 20:20:27 +0000

    Add missing MIME types

--- simplified-app/src/main/java/org/nypl/simplified/app/reader/ReaderHTTPMimeMap.java
+++ simplified-app/src/main/java/org/nypl/simplified/app/reader/ReaderHTTPMimeMap.java
@@ -24,11 +24,33 @@ public final class ReaderHTTPMimeMap implements ReaderHTTPMimeMapType
   {
     this.default_type = NullCheck.notNull(in_default_type);
     this.types = new ConcurrentHashMap<String, String>();
+    this.types.put("asc", "text/plain");
+    this.types.put("class", "application/octet-stream");
     this.types.put("css", "text/css");
+    this.types.put("doc", "application/msword");
+    this.types.put("exe", "application/octet-stream");
+    this.types.put("flv", "video/x-flv");
+    this.types.put("gif", "image/gif");
+    this.types.put("htm", "text/html");
     this.types.put("html", "text/html");
+    this.types.put("java", "text/x-java-source, text/java");
+    this.types.put("jpeg", "image/jpeg");
+    this.types.put("jpg", "image/jpeg");
     this.types.put("js", "application/javascript");
+    this.types.put("m3u", "audio/mpeg-url");
+    this.types.put("mov", "video/quicktime");
+    this.types.put("mp3", "audio/mpeg");
+    this.types.put("mp4", "video/mp4");
+    this.types.put("ogg", "application/x-ogg");
+    this.types.put("ogv", "video/ogg");
+    this.types.put("pdf", "application/pdf");
     this.types.put("png", "image/png");
+    this.types.put("swf", "application/x-shockwave-flash");
+    this.types.put("txt", "text/plain");
+    this.types.put("webm", "video/webm");
     this.types.put("xhtml", "application/xhtml+xml");
+    this.types.put("xml", "application/xml");
+    this.types.put("zip", "application/octet-stream");
   }
 
   @Override public String getDefaultMimeType()
