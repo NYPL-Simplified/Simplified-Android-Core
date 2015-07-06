17cc56be05dadd6d6c8633bc2d30f92f7f02f88a
Mark Raynsford <code@io7m.com>
2015-07-06 16:04:16 +0000

    Net provider interface

--- simplified-app/src/main/java/org/nypl/simplified/app/drm/DRMTestActivity.java
+++ simplified-app/src/main/java/org/nypl/simplified/app/drm/DRMTestActivity.java
@@ -9,6 +9,7 @@ import org.nypl.drm.core.AdobeAdeptConnectorFactory;
 import org.nypl.drm.core.AdobeAdeptConnectorFactoryType;
 import org.nypl.drm.core.AdobeAdeptConnectorType;
 import org.nypl.drm.core.AdobeAdeptDRMClientType;
+import org.nypl.drm.core.AdobeAdeptNetProvider;
 import org.nypl.drm.core.AdobeAdeptNetProviderType;
 import org.nypl.drm.core.AdobeAdeptResourceProviderType;
 import org.nypl.drm.core.DRMException;
@@ -17,6 +18,7 @@ import org.nypl.simplified.app.utilities.LogUtilities;
 import org.slf4j.Logger;
 
 import android.app.Activity;
+import android.os.AsyncTask;
 import android.os.Bundle;
 
 import com.io7m.jnull.Nullable;
@@ -25,8 +27,7 @@ import com.io7m.junreachable.UnreachableCodeException;
 
 public final class DRMTestActivity extends Activity implements
   AdobeAdeptResourceProviderType,
-  AdobeAdeptDRMClientType,
-  AdobeAdeptNetProviderType
+  AdobeAdeptDRMClientType
 {
   private static final Logger LOG;
 
@@ -39,27 +40,42 @@ public final class DRMTestActivity extends Activity implements
   {
     super.onCreate(state);
 
-    try {
-      final AdobeAdeptConnectorFactoryType f =
-        AdobeAdeptConnectorFactory.get();
+    final AsyncTask<Void, Void, Void> task =
+      new AsyncTask<Void, Void, Void>() {
+        @Override protected @Nullable Void doInBackground(
+          final @Nullable Void... params)
+        {
+          DRMTestActivity.LOG.debug("task started");
 
-      final AdobeAdeptConnectorType p =
-        f.get(
-          "org.nypl.simplified.app",
-          this,
-          this,
-          this,
-          "42f40c40374851a5b4a3d8375cb98924",
-          "NYPL Reader",
-          new File("/data/local/tmp/simplified"),
-          new File("/data/local/tmp/simplified"),
-          new File("/data/local/tmp/simplified"));
+          try {
+            final AdobeAdeptConnectorFactoryType f =
+              AdobeAdeptConnectorFactory.get();
+            final AdobeAdeptNetProviderType net =
+              AdobeAdeptNetProvider.get("Simplified");
 
-    } catch (final DRMUnsupportedException e) {
-      DRMTestActivity.LOG.error("unsupported drm: ", e);
-    } catch (final DRMException e) {
-      DRMTestActivity.LOG.error("error starting drm: ", e);
-    }
+            final AdobeAdeptConnectorType p =
+              f.get(
+                "org.nypl.simplified.app",
+                DRMTestActivity.this,
+                net,
+                "42f40c40374851a5b4a3d8375cb98924",
+                "NYPL Reader",
+                new File("/data/local/tmp/simplified"),
+                new File("/data/local/tmp/simplified"),
+                new File("/data/local/tmp/simplified"));
+
+          } catch (final DRMUnsupportedException e) {
+            DRMTestActivity.LOG.error("unsupported drm: ", e);
+          } catch (final DRMException e) {
+            DRMTestActivity.LOG.error("error starting drm: ", e);
+          }
+
+          return null;
+        }
+      };
+
+    DRMTestActivity.LOG.debug("starting task");
+    task.execute();
   }
 
   @Override public @Nullable byte[] getResourceAsBytes(
