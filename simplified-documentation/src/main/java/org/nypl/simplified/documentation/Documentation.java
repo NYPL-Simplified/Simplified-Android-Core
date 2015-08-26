d290b0ef4fcdacf7f18c9a9a9ae2e8111273c586
Mark Raynsford <code@io7m.com>
2015-08-26 11:03:04 +0000

    Use jstructural maven plugin to generate documentation

---
+++ simplified-documentation/src/main/java/org/nypl/simplified/documentation/Documentation.java
@@ -0,0 +1,13 @@
+package org.nypl.simplified.documentation;
+
+/**
+ * Marker class for looking up files by resource.
+ */
+
+public final class Documentation
+{
+  private Documentation()
+  {
+    throw new AssertionError("Unreachable code");
+  }
+}
