4807543b562aedcdb235ea2c480c824640514031
Aferdita Muriqi <aferditamuriqi@nypl.org>
2016-08-03 12:27:59 -0400

    Split logout and deactivation into separate tasks, same as it is being done on login and activation. requesting  adobe auth data token to deactivate device

---
+++ simplified-books-core/src/main/java/org/nypl/simplified/books/core/BooksControllerDeviceDeActivationTask.java
@@ -0,0 +1,150 @@
+package org.nypl.simplified.books.core;
+
+import com.io7m.jfunctional.None;
+import com.io7m.jfunctional.Option;
+import com.io7m.jfunctional.OptionType;
+import com.io7m.jfunctional.OptionVisitorType;
+import com.io7m.jfunctional.Some;
+import com.io7m.jfunctional.Unit;
+import com.io7m.jnull.NullCheck;
+
+import org.nypl.drm.core.AdobeAdeptActivationReceiverType;
+import org.nypl.drm.core.AdobeAdeptConnectorType;
+import org.nypl.drm.core.AdobeAdeptDeactivationReceiverType;
+import org.nypl.drm.core.AdobeAdeptExecutorType;
+import org.nypl.drm.core.AdobeAdeptProcedureType;
+import org.nypl.drm.core.AdobeDeviceID;
+import org.nypl.drm.core.AdobeUserID;
+import org.nypl.drm.core.AdobeVendorID;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Runnable that JUST activates the device with Adobe (used on startup, and as part of logging in)
+ */
+
+public class BooksControllerDeviceDeActivationTask implements Runnable,
+  AdobeAdeptDeactivationReceiverType {
+
+  private final OptionType<AdobeAdeptExecutorType> adobe_drm;
+  private final AccountCredentials credentials;
+
+  private static final Logger LOG;
+
+  static {
+    LOG =  LogUtilities.getLog(BooksControllerDeviceDeActivationTask.class);
+  }
+
+  BooksControllerDeviceDeActivationTask(
+    final OptionType<AdobeAdeptExecutorType> in_adobe_drm,
+    final AccountCredentials in_credentials) {
+    this.adobe_drm = in_adobe_drm;
+    this.credentials = in_credentials;
+  }
+
+  @Override
+  public void run() {
+    if (this.adobe_drm.isSome()) {
+      final Some<AdobeAdeptExecutorType> some =
+        (Some<AdobeAdeptExecutorType>) this.adobe_drm;
+      final AdobeAdeptExecutorType adobe_exec = some.get();
+
+      final AccountBarcode user = this.credentials.getUser();
+      final AccountPIN pass = this.credentials.getPassword();
+      final OptionType<AccountAdobeToken> adobe_token = this.credentials.getAdobeToken();
+      final OptionType<AdobeVendorID> vendor_opt = this.credentials.getAdobeVendor();
+      final OptionType<AdobeUserID> user_id = this.credentials.getAdobeUserID();
+
+      vendor_opt.accept(
+        new OptionVisitorType<AdobeVendorID, Unit>() {
+          @Override
+          public Unit none(final None<AdobeVendorID> n) {
+            BooksControllerDeviceDeActivationTask.this.onDeactivationError(
+              "No Adobe vendor ID provided");
+            return Unit.unit();
+          }
+
+
+          @Override
+          public Unit some(final Some<AdobeVendorID> s) {
+            adobe_exec.execute(
+              new AdobeAdeptProcedureType() {
+                @Override
+                public void executeWith(final AdobeAdeptConnectorType c) {
+                  final AdobeUserID[] user_id = new AdobeUserID[1];
+
+                  c.getDeviceActivations(new AdobeAdeptActivationReceiverType() {
+                    @Override
+                    public void onActivationsCount(final int count) {
+                      if (count == 0) {
+                        BooksControllerDeviceDeActivationTask.this.onDeactivationError("No devices were activated.");
+                      }
+                    }
+
+                    @Override
+                    public void onActivation(final int index,
+                                             final AdobeVendorID authority,
+                                             final String device_id,
+                                             final String user_name,
+                                             final AdobeUserID in_user_id,
+                                             final String expires) {
+                      user_id[0] = in_user_id;
+                    }
+
+                    @Override
+                    public void onActivationError(final String message) {
+                      BooksControllerDeviceDeActivationTask.this.onDeactivationError("Activation error when getting existing activations. This should never happen.");
+                    }
+                  });
+
+//                  if (user_id.isSome()){
+                  if (user_id[0] != null) {
+
+                    if (BooksControllerDeviceDeActivationTask.this.credentials.getAuthToken().isNone()) {
+                      c.deactivateDevice(
+                        BooksControllerDeviceDeActivationTask.this,
+                        s.get(),
+                        user_id[0],
+                        user.toString(),
+                        pass.toString());
+                    } else {
+                      c.deactivateDevice(
+                        BooksControllerDeviceDeActivationTask.this,
+                        s.get(),
+//                        ((Some<AdobeUserID>)user_id).get(),
+                        user_id[0],
+                        ((Some<AccountAdobeToken>)adobe_token).get().toString(),
+                        "");
+                    }
+
+                    BooksControllerDeviceDeActivationTask.this.credentials.setAdobeUserID(Option.<AdobeUserID>none());
+                    BooksControllerDeviceDeActivationTask.this.credentials.setAdobeDeviceID(Option.<AdobeDeviceID>none());
+
+                  }
+
+
+
+                }
+              });
+            return Unit.unit();
+          }
+
+
+        });
+    }
+  }
+
+
+  @Override
+  public void onDeactivationError(final String message) {
+    BooksControllerDeviceDeActivationTask.LOG.debug("Failed to deactivate device: {}", message);
+  }
+
+  @Override
+  public void onDeactivationSucceeded() {
+    /**
+     * Device deactivation succeeded.
+     */
+  }
+
+}
--- simplified-books-core/src/main/java/org/nypl/simplified/books/core/BooksControllerLogoutTask.java
+++ simplified-books-core/src/main/java/org/nypl/simplified/books/core/BooksControllerLogoutTask.java
@@ -1,158 +1,190 @@
 package org.nypl.simplified.books.core;
 
-import com.io7m.jfunctional.None;
 import com.io7m.jfunctional.Option;
 import com.io7m.jfunctional.OptionType;
-import com.io7m.jfunctional.OptionVisitorType;
 import com.io7m.jfunctional.Some;
 import com.io7m.jfunctional.Unit;
 import com.io7m.jnull.NullCheck;
+import com.io7m.junreachable.UnreachableCodeException;
 
-import org.nypl.drm.core.AdobeAdeptActivationReceiverType;
-import org.nypl.drm.core.AdobeAdeptConnectorType;
-import org.nypl.drm.core.AdobeAdeptDeactivationReceiverType;
 import org.nypl.drm.core.AdobeAdeptExecutorType;
-import org.nypl.drm.core.AdobeAdeptProcedureType;
-import org.nypl.drm.core.AdobeUserID;
-import org.nypl.drm.core.AdobeVendorID;
+import org.nypl.simplified.http.core.HTTPAuthBasic;
+import org.nypl.simplified.http.core.HTTPAuthOAuth;
+import org.nypl.simplified.http.core.HTTPAuthType;
+import org.nypl.simplified.http.core.HTTPResultError;
+import org.nypl.simplified.http.core.HTTPResultException;
+import org.nypl.simplified.http.core.HTTPResultMatcherType;
+import org.nypl.simplified.http.core.HTTPResultOKType;
+import org.nypl.simplified.http.core.HTTPResultType;
+import org.nypl.simplified.http.core.HTTPType;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
+import java.io.InputStream;
+import java.net.URI;
+import java.util.Scanner;
 
-final class BooksControllerLogoutTask implements Runnable,
-  AdobeAdeptDeactivationReceiverType
-{
+final class BooksControllerLogoutTask implements Runnable {
   private static final Logger LOG;
 
   static {
-    LOG = NullCheck.notNull(
-      LoggerFactory.getLogger(BooksControllerLogoutTask.class));
+    LOG =  LogUtilities.getLog(BooksControllerLogoutTask.class);
   }
 
-  private final AccountLogoutListenerType          listener;
+  private final AccountLogoutListenerType listener;
   private final OptionType<AdobeAdeptExecutorType> adobe_drm;
-  private final BookDatabaseType                   database;
-  private final AccountsDatabaseType               accounts_database;
+  private final BookDatabaseType database;
+  private final AccountsDatabaseType accounts_database;
+  private final BooksControllerConfigurationType config;
+  private final HTTPType http;
+  private final AccountCredentials credentials;
+
 
   BooksControllerLogoutTask(
     final BookDatabaseType in_book_database,
     final AccountsDatabaseType in_accounts_database,
     final OptionType<AdobeAdeptExecutorType> in_adobe_drm,
-    final AccountLogoutListenerType in_listener)
-  {
+    final AccountLogoutListenerType in_listener,
+    final BooksControllerConfigurationType in_config,
+    final HTTPType in_http,
+    final AccountCredentials in_credentials) {
     this.database = NullCheck.notNull(in_book_database);
     this.adobe_drm = NullCheck.notNull(in_adobe_drm);
     this.accounts_database = NullCheck.notNull(in_accounts_database);
     this.listener = new AccountLogoutListenerCatcher(
       BooksControllerLogoutTask.LOG, NullCheck.notNull(in_listener));
+    this.config = NullCheck.notNull(in_config);
+    this.http = NullCheck.notNull(in_http);
+    this.credentials = NullCheck.notNull(in_credentials);
+
   }
 
-  @Override public void run()
-  {
-    try {
 
-      final OptionType<AccountCredentials> credentials_opt = this.accounts_database.accountGetCredentials();
-      if (this.adobe_drm.isSome() && credentials_opt.isSome()) {
-        final AccountCredentials credentials = ((Some<AccountCredentials>) credentials_opt).get();
-        final Some<AdobeAdeptExecutorType> some =
-          (Some<AdobeAdeptExecutorType>) this.adobe_drm;
-        final AccountBarcode user = credentials.getUser();
-        final AccountPIN pass = credentials.getPassword();
-        final OptionType<AdobeVendorID> vendor_opt = credentials.getAdobeVendor();
-        final AdobeAdeptExecutorType adobe_exec = some.get();
+  private <T> void onHTTPException(final HTTPResultException<T> e) {
+    final Exception ex = e.getError();
+    BooksControllerLogoutTask.this.listener.onAccountLogoutFailure(Option.<Throwable>some(ex), ex.getMessage());
+  }
 
-        vendor_opt.accept(
-          new OptionVisitorType<AdobeVendorID, Unit>()
-          {
-            @Override
-            public Unit none(final None<AdobeVendorID> n)
-            {
-              BooksControllerLogoutTask.this.onDeactivationError(
-                "No Adobe vendor ID provided");
-              return Unit.unit();
-            }
+  private void onHTTPServerReturnedError(final HTTPResultError<?> e) {
+    final int code = e.getStatus();
+    BooksControllerLogoutTask.this.listener.onAccountLogoutFailureServerError(code);
+  }
 
-            @Override
-            public Unit some(final Some<AdobeVendorID> s)
-            {
-              adobe_exec.execute(
-                new AdobeAdeptProcedureType()
-                {
-                  @Override
-                  public void executeWith(final AdobeAdeptConnectorType c)
-                  {
-                    final AdobeUserID[] user_id = new AdobeUserID[1];
+  private void onHTTPServerAcceptedCredentials(final InputStream data) {
+    /**
+     * If an Adobe DRM implementation is available, activate the device
+     * with the credentials. If the Adobe server rejects the credentials,
+     * then the login attempt is still considered to have failed.
+     */
 
-                    c.getDeviceActivations(new AdobeAdeptActivationReceiverType()
-                    {
-                      @Override
-                      public void onActivationsCount(final int count)
-                      {
-                        if (count == 0) {
-                          BooksControllerLogoutTask.this.onDeactivationError("No devices were activated.");
-                        }
-                      }
+    if (this.adobe_drm.isSome()) {
 
-                      @Override
-                      public void onActivation(final int index,
-                                               final AdobeVendorID authority,
-                                               final String device_id,
-                                               final String user_name,
-                                               final AdobeUserID in_user_id,
-                                               final String expires)
-                      {
-                        user_id[0] = in_user_id;
-                      }
+/* remove this , once permanent token is implemented*/
+      Scanner scanner = new Scanner(data).useDelimiter("\\A");
+      String adobe_token = scanner.hasNext() ? scanner.next() : "";
+      BooksControllerLogoutTask.LOG.debug("adobe temporary token: {}", adobe_token);
+      this.credentials.setAdobeToken(Option.some(new AccountAdobeToken(adobe_token)));
+/**/
 
-                      @Override
-                      public void onActivationError(final String message)
-                      {
-                        BooksControllerLogoutTask.this.onDeactivationError("Activation error when getting existing activations. This should never happen.");
-                      }
-                    });
+      BooksControllerDeviceDeActivationTask device_deactivationTask = new BooksControllerDeviceDeActivationTask(this.adobe_drm,
+        this.credentials) {
 
-                    if (user_id[0] != null) {
-                      c.deactivateDevice(
-                        BooksControllerLogoutTask.this,
-                        s.get(),
-                        user_id[0],
-                        user.toString(),
-                        pass.toString());
-                    }
-                  }
-                });
-              return Unit.unit();
-            }
-          });
-      }
+        @Override
+        public void onDeactivationError(final String message) {
+          BooksControllerLogoutTask.this.listener.onAccountLogoutFailure(Option.<Throwable>none(), message);
+        }
 
-    } catch (final Throwable e) {
-      this.listener.onAccountLogoutFailure(
-        Option.some(e), NullCheck.notNull(e.getMessage()));
+        @Override
+        public void onDeactivationSucceeded() {
+          /**
+           * Delete the books database.
+           */
+
+          BooksControllerLogoutTask.this.onDeactivationSucceeded();
+
+        }
+
+      };
+      device_deactivationTask.run();
+
+    } else {
+
+      /**
+       * Otherwise, the login process is completed.
+       */
+
+      this.onDeactivationSucceeded();
     }
   }
 
-  @Override
-  public void onDeactivationError(final String message)
-  {
-    this.listener.onAccountLogoutFailure(Option.<Throwable>none(), message);
-  }
-
-  @Override
-  public void onDeactivationSucceeded()
-  {
+  public void onDeactivationSucceeded() {
     /**
      * Delete the books database.
      */
 
     try {
-      this.accounts_database.accountRemoveCredentials();
-      this.database.databaseDestroy();
-      this.listener.onAccountLogoutSuccess();
+      BooksControllerLogoutTask.this.accounts_database.accountRemoveCredentials();
+      BooksControllerLogoutTask.this.database.databaseDestroy();
+      BooksControllerLogoutTask.this.listener.onAccountLogoutSuccess();
     } catch (IOException e) {
       e.printStackTrace();
-      this.listener.onAccountLogoutFailure(Option.<Throwable>some(e), e.getMessage());
+      BooksControllerLogoutTask.this.listener.onAccountLogoutFailure(Option.<Throwable>some(e), e.getMessage());
     }
+  }
+
+
+  @Override
+  public void run() {
+
+
+    final AccountBarcode user = this.credentials.getUser();
+    final AccountPIN pass = this.credentials.getPassword();
+    HTTPAuthType auth =
+      new HTTPAuthBasic(user.toString(), pass.toString());
+
+    if (credentials.getAuthToken().isSome()) {
+      final AccountAuthToken token = ((Some<AccountAuthToken>) credentials.getAuthToken()).get();
+      if (token != null) {
+        auth = new HTTPAuthOAuth(token.toString());
+      }
+    }
+
+
+    URI auth_uri = this.config.getCurrentLoansURI();
+    HTTPResultType<InputStream> r;
+    if (this.adobe_drm.isSome()) {
+      auth_uri = this.config.getCurrentRootFeedURI().resolve("AdobeAuth/authdata");
+      r = this.http.get(Option.some(auth), auth_uri, 0);
+    } else {
+      r = this.http.head(Option.some(auth), auth_uri);
+    }
+
+    BooksControllerLogoutTask.LOG.debug(
+      "attempting login on {}", auth_uri);
+
+
+    r.matchResult(
+      new HTTPResultMatcherType<InputStream, Unit, UnreachableCodeException>() {
+        @Override
+        public Unit onHTTPError(final HTTPResultError<InputStream> e) {
+          BooksControllerLogoutTask.this.onHTTPServerReturnedError(e);
+          return Unit.unit();
+        }
+
+        @Override
+        public Unit onHTTPException(final HTTPResultException<InputStream> e) {
+          BooksControllerLogoutTask.this.onHTTPException(e);
+          return Unit.unit();
+        }
+
+        @Override
+        public Unit onHTTPOK(final HTTPResultOKType<InputStream> e) {
+          BooksControllerLogoutTask.this.onHTTPServerAcceptedCredentials(e.getValue());
+          return Unit.unit();
+        }
+      });
+
+
   }
 }
