bedb025853d0c45eb06225b8010974105b983d62
Aferdita Muriqi <aferditamuriqi@nypl.org>
2016-08-16 18:45:41 -0400

    hide return button if account is not ready, can't return a book if account is not active

--- simplified-app-shared/src/main/java/org/nypl/simplified/app/catalog/CatalogBookRevokeButton.java
+++ simplified-app-shared/src/main/java/org/nypl/simplified/app/catalog/CatalogBookRevokeButton.java
@@ -4,11 +4,16 @@ import android.app.Activity;
 import android.app.FragmentManager;
 import android.content.res.Resources;
 import android.view.View;
+
+import com.io7m.jfunctional.OptionType;
+import com.io7m.jfunctional.Some;
 import com.io7m.jnull.NullCheck;
 import com.io7m.jnull.Nullable;
+
 import org.nypl.simplified.app.R;
 import org.nypl.simplified.app.Simplified;
 import org.nypl.simplified.app.SimplifiedCatalogAppServicesType;
+import org.nypl.simplified.books.core.BookDatabaseEntrySnapshot;
 import org.nypl.simplified.books.core.BookID;
 import org.nypl.simplified.books.core.BooksType;
 
@@ -31,56 +36,68 @@ public final class CatalogBookRevokeButton extends CatalogLeftPaddedButton
   public CatalogBookRevokeButton(
     final Activity in_activity,
     final BookID in_book_id,
-    final CatalogBookRevokeType in_revoke_type)
+    final CatalogBookRevokeType in_revoke_type,
+    final BooksType in_books)
   {
     super(in_activity);
     NullCheck.notNull(in_book_id);
     NullCheck.notNull(in_revoke_type);
 
-    final Resources rr = NullCheck.notNull(in_activity.getResources());
-    switch (in_revoke_type) {
-      case REVOKE_LOAN: {
-        this.getTextView().setText(
-          NullCheck.notNull(rr.getString(R.string.catalog_book_revoke_loan)));
-        this.getTextView().setContentDescription(
-          NullCheck.notNull(rr.getString(R.string.catalog_accessibility_book_revoke_loan)));
-        break;
+    final OptionType<BookDatabaseEntrySnapshot> snap_opt =
+      in_books.bookGetDatabase().databaseGetEntrySnapshot(in_book_id);
+
+    if(in_books.accountIsDeviceActive() || ((Some<BookDatabaseEntrySnapshot>) snap_opt).get().getAdobeRights().isNone()) {
+
+      final Resources rr = NullCheck.notNull(in_activity.getResources());
+
+      this.getTextView().setTextSize(12.0f);
+      this.setBackground(rr.getDrawable(R.drawable.simplified_button));
+      this.getTextView().setTextColor(rr.getColorStateList(R.drawable.simplified_button_text));
+
+      switch (in_revoke_type) {
+        case REVOKE_LOAN: {
+          this.getTextView().setText(
+            NullCheck.notNull(rr.getString(R.string.catalog_book_revoke_loan)));
+          this.getTextView().setContentDescription(
+            NullCheck.notNull(rr.getString(R.string.catalog_accessibility_book_revoke_loan)));
+          break;
+        }
+        case REVOKE_HOLD: {
+          this.getTextView().setText(
+            NullCheck.notNull(rr.getString(R.string.catalog_book_revoke_hold)));
+          this.getTextView().setContentDescription(
+            NullCheck.notNull(rr.getString(R.string.catalog_accessibility_book_revoke_hold)));
+          break;
+        }
       }
-      case REVOKE_HOLD: {
-        this.getTextView().setText(
-          NullCheck.notNull(rr.getString(R.string.catalog_book_revoke_hold)));
-        this.getTextView().setContentDescription(
-          NullCheck.notNull(rr.getString(R.string.catalog_accessibility_book_revoke_hold)));
-        break;
-      }
+
+      this.setOnClickListener(
+        new OnClickListener() {
+          @Override
+          public void onClick(
+            final @Nullable View v) {
+            final CatalogBookRevokeDialog d =
+              CatalogBookRevokeDialog.newDialog(in_revoke_type);
+            d.setOnConfirmListener(
+              new Runnable() {
+                @Override
+                public void run() {
+                  final SimplifiedCatalogAppServicesType app =
+                    Simplified.getCatalogAppServices();
+                  final BooksType books = app.getBooks();
+                  books.bookRevoke(in_book_id);
+                }
+              });
+            final FragmentManager fm = in_activity.getFragmentManager();
+            d.show(fm, "revoke-confirm");
+          }
+        });
+    }
+    else
+    {
+      super.removeView(this);
+
     }
 
-    this.getTextView().setTextSize(12.0f);
-    this.setBackground(rr.getDrawable(R.drawable.simplified_button));
-    this.getTextView().setTextColor(rr.getColorStateList(R.drawable.simplified_button_text));
-
-    this.setOnClickListener(
-      new OnClickListener()
-      {
-        @Override public void onClick(
-          final @Nullable View v)
-        {
-          final CatalogBookRevokeDialog d =
-            CatalogBookRevokeDialog.newDialog(in_revoke_type);
-          d.setOnConfirmListener(
-            new Runnable()
-            {
-              @Override public void run()
-              {
-                final SimplifiedCatalogAppServicesType app =
-                  Simplified.getCatalogAppServices();
-                final BooksType books = app.getBooks();
-                books.bookRevoke(in_book_id);
-              }
-            });
-          final FragmentManager fm = in_activity.getFragmentManager();
-          d.show(fm, "revoke-confirm");
-        }
-      });
   }
 }
