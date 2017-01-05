115170ec8a556fa5bff9b7cab21ba8fa36a70623
Aferdita Muriqi <aferditamuriqi@nypl.org>
2017-01-05 14:10:26 -0500

    fixed acquisition parsing

--- simplified-opds-core/src/main/java/org/nypl/simplified/opds/core/OPDSAcquisitionFeedEntryParser.java
+++ simplified-opds-core/src/main/java/org/nypl/simplified/opds/core/OPDSAcquisitionFeedEntryParser.java
@@ -282,22 +282,23 @@ public final class OPDSAcquisitionFeedEntryParser
     final List<Element> top_level_list = OPDSXML.getChildElementsWithName(
       link, OPDSFeedConstants.OPDS_URI, "indirectAcquisition");
 
-    if (top_level_list.size() == 0) {
-      return true;
-    }
-
-    for (Element top_level_e : top_level_list) {
-      if ("vnd.adobe/adept+xml".equals(top_level_e.getAttribute("type"))) {
-        final List<Element> second_level_list = OPDSXML.getChildElementsWithName(
-          link, OPDSFeedConstants.OPDS_URI, "indirectAcquisition");
-        for (Element second_level_e : second_level_list) {
-          if ("application/epub+zip".equals(second_level_e.getAttribute("type"))) {
-            return true;
+    if (top_level_list.size() != 0) {
+      for (Element top_level_e : top_level_list) {
+        if ("vnd.adobe/adept+xml".equals(top_level_e.getAttribute("type"))) {
+          final List<Element> second_level_list = OPDSXML.getChildElementsWithName(
+            link, OPDSFeedConstants.OPDS_URI, "indirectAcquisition");
+          for (Element second_level_e : second_level_list) {
+            if ("application/epub+zip".equals(second_level_e.getAttribute("type"))) {
+              return true;
+            }
           }
+        } else if ("application/epub+zip".equals(top_level_e.getAttribute("type"))) {
+          return true;
         }
-      } else if ("application/epub+zip".equals(top_level_e.getAttribute("type"))) {
-        return true;
       }
+    }
+    else if ("application/epub+zip".equals(link.getAttribute("type"))) {
+      return true;
     }
 
     return false;
