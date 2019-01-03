736700a5b99c0299d4d4545f2404205b6408cd48
Mark Raynsford <code@io7m.com>
2019-01-03 15:19:57 +0000

    Import improved JSON utilities from LFA

--- simplified-json-core/src/main/java/org/nypl/simplified/json/core/JSONParserUtilities.java
+++ simplified-json-core/src/main/java/org/nypl/simplified/json/core/JSONParserUtilities.java
@@ -3,12 +3,16 @@ package org.nypl.simplified.json.core;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.node.ArrayNode;
 import com.fasterxml.jackson.databind.node.ObjectNode;
+import com.io7m.jfunctional.None;
 import com.io7m.jfunctional.Option;
 import com.io7m.jfunctional.OptionType;
+import com.io7m.jfunctional.OptionVisitorType;
 import com.io7m.jfunctional.PartialFunctionType;
+import com.io7m.jfunctional.Some;
 import com.io7m.jnull.NullCheck;
 import com.io7m.jnull.Nullable;
 import com.io7m.junreachable.UnreachableCodeException;
+
 import org.nypl.simplified.rfc3339.core.RFC3339Formatter;
 
 import java.math.BigInteger;
@@ -19,15 +23,13 @@ import java.util.Calendar;
 
 /**
  * <p>Utility functions for deserializing elements from JSON.</p>
- *
+ * <p>
  * <p>The functions take a strict approach: Types are checked upon key retrieval
  * and exceptions are raised if the type is not exactly as expected.</p>
  */
 
-public final class JSONParserUtilities
-{
-  private JSONParserUtilities()
-  {
+public final class JSONParserUtilities {
+  private JSONParserUtilities() {
     throw new UnreachableCodeException();
   }
 
@@ -36,17 +38,15 @@ public final class JSONParserUtilities
    *
    * @param key An optional advisory key to be used in error messages
    * @param n   A node
-   *
    * @return {@code n} as an {@link ObjectNode}
-   *
    * @throws JSONParseException On type errors
    */
 
   public static ObjectNode checkObject(
-    final @Nullable String key,
-    final JsonNode n)
-    throws JSONParseException
-  {
+      final @Nullable String key,
+      final JsonNode n)
+      throws JSONParseException {
+
     NullCheck.notNull(n);
 
     switch (n.getNodeType()) {
@@ -85,19 +85,68 @@ public final class JSONParserUtilities
   }
 
   /**
+   * Check that {@code n} is an array.
+   *
+   * @param key An optional advisory key to be used in error messages
+   * @param n   A node
+   * @return {@code n} as an {@link ObjectNode}
+   * @throws JSONParseException On type errors
+   */
+
+  public static ArrayNode checkArray(
+      final @Nullable String key,
+      final JsonNode n)
+      throws JSONParseException {
+
+    NullCheck.notNull(n);
+
+    switch (n.getNodeType()) {
+      case ARRAY: {
+        return (ArrayNode) n;
+      }
+      case BINARY:
+      case BOOLEAN:
+      case MISSING:
+      case NULL:
+      case NUMBER:
+      case POJO:
+      case OBJECT:
+      case STRING: {
+        final StringBuilder sb = new StringBuilder(128);
+        if (key != null) {
+          sb.append("Expected: A key '");
+          sb.append(key);
+          sb.append("' with a value of type Object\n");
+          sb.append("Got: A value of type ");
+          sb.append(n.getNodeType());
+          sb.append("\n");
+        } else {
+          sb.append("Expected: A value of type Object\n");
+          sb.append("Got: A value of type ");
+          sb.append(n.getNodeType());
+          sb.append("\n");
+        }
+
+        final String m = NullCheck.notNull(sb.toString());
+        throw new JSONParseException(m);
+      }
+    }
+
+    throw new UnreachableCodeException();
+  }
+
+  /**
    * @param key A key assumed to be holding a value
    * @param s   A node
-   *
    * @return An array from key {@code key}
-   *
    * @throws JSONParseException On type errors
    */
 
   public static ArrayNode getArray(
-    final ObjectNode s,
-    final String key)
-    throws JSONParseException
-  {
+      final ObjectNode s,
+      final String key)
+      throws JSONParseException {
+
     NullCheck.notNull(s);
     NullCheck.notNull(key);
 
@@ -132,17 +181,15 @@ public final class JSONParserUtilities
   /**
    * @param key A key assumed to be holding a value
    * @param o   A node
-   *
    * @return A boolean value from key {@code key}
-   *
    * @throws JSONParseException On type errors
    */
 
   public static boolean getBoolean(
-    final ObjectNode o,
-    final String key)
-    throws JSONParseException
-  {
+      final ObjectNode o,
+      final String key)
+      throws JSONParseException {
+
     NullCheck.notNull(o);
     NullCheck.notNull(key);
 
@@ -177,17 +224,15 @@ public final class JSONParserUtilities
   /**
    * @param key A key assumed to be holding a value
    * @param n   A node
-   *
    * @return An integer value from key {@code key}
-   *
    * @throws JSONParseException On type errors
    */
 
   public static int getInteger(
-    final ObjectNode n,
-    final String key)
-    throws JSONParseException
-  {
+      final ObjectNode n,
+      final String key)
+      throws JSONParseException {
+
     NullCheck.notNull(n);
     NullCheck.notNull(key);
 
@@ -221,18 +266,59 @@ public final class JSONParserUtilities
 
   /**
    * @param key A key assumed to be holding a value
+   * @param n   A node
+   * @return A double value from key {@code key}
+   * @throws JSONParseException On type errors
+   */
+
+  public static double getDouble(
+      final ObjectNode n,
+      final String key)
+      throws JSONParseException {
+
+    NullCheck.notNull(n);
+    NullCheck.notNull(key);
+
+    final JsonNode v = JSONParserUtilities.getNode(n, key);
+    switch (v.getNodeType()) {
+      case ARRAY:
+      case BINARY:
+      case BOOLEAN:
+      case MISSING:
+      case NULL:
+      case OBJECT:
+      case POJO:
+      case STRING: {
+        final StringBuilder sb = new StringBuilder(128);
+        sb.append("Expected: A key '");
+        sb.append(key);
+        sb.append("' with a value of type Double\n");
+        sb.append("Got: A value of type ");
+        sb.append(v.getNodeType());
+        sb.append("\n");
+        final String m = NullCheck.notNull(sb.toString());
+        throw new JSONParseException(m);
+      }
+      case NUMBER: {
+        return v.asDouble();
+      }
+    }
+
+    throw new UnreachableCodeException();
+  }
+
+  /**
+   * @param key A key assumed to be holding a value
    * @param s   A node
-   *
    * @return An arbitrary json node from key {@code key}
-   *
    * @throws JSONParseException On type errors
    */
 
   public static JsonNode getNode(
-    final ObjectNode s,
-    final String key)
-    throws JSONParseException
-  {
+      final ObjectNode s,
+      final String key)
+      throws JSONParseException {
+
     NullCheck.notNull(s);
     NullCheck.notNull(key);
 
@@ -252,17 +338,15 @@ public final class JSONParserUtilities
   /**
    * @param key A key assumed to be holding a value
    * @param s   A node
-   *
    * @return An object value from key {@code key}
-   *
    * @throws JSONParseException On type errors
    */
 
   public static ObjectNode getObject(
-    final ObjectNode s,
-    final String key)
-    throws JSONParseException
-  {
+      final ObjectNode s,
+      final String key)
+      throws JSONParseException {
+
     NullCheck.notNull(s);
     NullCheck.notNull(key);
 
@@ -273,17 +357,15 @@ public final class JSONParserUtilities
   /**
    * @param key A key assumed to be holding a value
    * @param s   A node
-   *
    * @return An object value from key {@code key}, if the key exists
-   *
    * @throws JSONParseException On type errors
    */
 
   public static OptionType<ObjectNode> getObjectOptional(
-    final ObjectNode s,
-    final String key)
-    throws JSONParseException
-  {
+      final ObjectNode s,
+      final String key)
+      throws JSONParseException {
+
     NullCheck.notNull(s);
     NullCheck.notNull(key);
 
@@ -296,17 +378,15 @@ public final class JSONParserUtilities
   /**
    * @param key A key assumed to be holding a value
    * @param s   A node
-   *
    * @return A string value from key {@code key}
-   *
    * @throws JSONParseException On type errors
    */
 
   public static String getString(
-    final ObjectNode s,
-    final String key)
-    throws JSONParseException
-  {
+      final ObjectNode s,
+      final String key)
+      throws JSONParseException {
+
     NullCheck.notNull(s);
     NullCheck.notNull(key);
 
@@ -341,17 +421,15 @@ public final class JSONParserUtilities
   /**
    * @param key A key assumed to be holding a value
    * @param n   A node
-   *
    * @return An integer value from key {@code key}, if the key exists
-   *
    * @throws JSONParseException On type errors
    */
 
   public static OptionType<Integer> getIntegerOptional(
-    final ObjectNode n,
-    final String key)
-    throws JSONParseException
-  {
+      final ObjectNode n,
+      final String key)
+      throws JSONParseException {
+
     NullCheck.notNull(n);
     NullCheck.notNull(key);
 
@@ -364,17 +442,66 @@ public final class JSONParserUtilities
   /**
    * @param key A key assumed to be holding a value
    * @param n   A node
-   *
+   * @return An double value from key {@code key}, if the key exists
+   * @throws JSONParseException On type errors
+   */
+
+  public static OptionType<Double> getDoubleOptional(
+      final ObjectNode n,
+      final String key)
+      throws JSONParseException {
+
+    NullCheck.notNull(n);
+    NullCheck.notNull(key);
+
+    if (n.has(key)) {
+      return Option.some(JSONParserUtilities.getDouble(n, key));
+    }
+    return Option.none();
+  }
+
+  /**
+   * @param key A key assumed to be holding a value
+   * @param n   A node
+   * @return An double value from key {@code key}, if the key exists, or {@code default_value} otherwise.
+   * @throws JSONParseException On type errors
+   */
+
+  public static double getDoubleDefault(
+      final ObjectNode n,
+      final String key,
+      final double default_value)
+      throws JSONParseException {
+
+    NullCheck.notNull(n);
+    NullCheck.notNull(key);
+
+    return getDoubleOptional(n, key).accept(
+        new OptionVisitorType<Double, Double>() {
+          @Override
+          public Double none(final None<Double> none) {
+            return default_value;
+          }
+
+          @Override
+          public Double some(final Some<Double> some) {
+            return some.get();
+          }
+        });
+  }
+
+  /**
+   * @param key A key assumed to be holding a value
+   * @param n   A node
    * @return A string value from key {@code key}, if the key exists
-   *
    * @throws JSONParseException On type errors
    */
 
   public static OptionType<String> getStringOptional(
-    final ObjectNode n,
-    final String key)
-    throws JSONParseException
-  {
+      final ObjectNode n,
+      final String key)
+      throws JSONParseException {
+
     NullCheck.notNull(n);
     NullCheck.notNull(key);
 
@@ -386,27 +513,55 @@ public final class JSONParserUtilities
 
   /**
    * @param key A key assumed to be holding a value
+   * @param n   A node
+   * @return A string value from key {@code key}, if the key exists, or {@code default_value} otherwise.
+   * @throws JSONParseException On type errors
+   */
+
+  public static String getStringDefault(
+      final ObjectNode n,
+      final String key,
+      final String default_value)
+      throws JSONParseException {
+
+    NullCheck.notNull(n);
+    NullCheck.notNull(key);
+
+    return getStringOptional(n, key).accept(
+        new OptionVisitorType<String, String>() {
+          @Override
+          public String none(final None<String> none) {
+            return default_value;
+          }
+
+          @Override
+          public String some(final Some<String> some) {
+            return some.get();
+          }
+        });
+  }
+
+  /**
+   * @param key A key assumed to be holding a value
    * @param s   A node
-   *
    * @return A timestamp value from key {@code key}
-   *
    * @throws JSONParseException On type errors
    */
 
   public static Calendar getTimestamp(
-    final ObjectNode s,
-    final String key)
-    throws JSONParseException
-  {
+      final ObjectNode s,
+      final String key)
+      throws JSONParseException {
+
     NullCheck.notNull(s);
     NullCheck.notNull(key);
 
     try {
       return RFC3339Formatter.parseRFC3339Date(
-        JSONParserUtilities.getString(s, key));
+          JSONParserUtilities.getString(s, key));
     } catch (final ParseException e) {
       final String m = NullCheck.notNull(
-        String.format("Could not parse RFC3999 date for key '%s'", key));
+          String.format("Could not parse RFC3999 date for key '%s'", key));
       throw new JSONParseException(m, e);
     }
   }
@@ -414,17 +569,15 @@ public final class JSONParserUtilities
   /**
    * @param key A key assumed to be holding a value
    * @param n   A node
-   *
    * @return A timestamp value from key {@code key}, if the key exists
-   *
    * @throws JSONParseException On type errors
    */
 
   public static OptionType<Calendar> getTimestampOptional(
-    final ObjectNode n,
-    final String key)
-    throws JSONParseException
-  {
+      final ObjectNode n,
+      final String key)
+      throws JSONParseException {
+
     NullCheck.notNull(n);
     NullCheck.notNull(key);
 
@@ -437,49 +590,44 @@ public final class JSONParserUtilities
   /**
    * @param key A key assumed to be holding a value
    * @param n   A node
-   *
    * @return A URI value from key {@code key}, if the key exists
-   *
    * @throws JSONParseException On type errors
    */
 
   public static OptionType<URI> getURIOptional(
-    final ObjectNode n,
-    final String key)
-    throws JSONParseException
-  {
+      final ObjectNode n,
+      final String key)
+      throws JSONParseException {
+
     NullCheck.notNull(n);
     NullCheck.notNull(key);
 
     return JSONParserUtilities.getStringOptional(n, key).mapPartial(
-      new PartialFunctionType<String, URI, JSONParseException>()
-      {
-        @Override public URI call(final String x)
-          throws JSONParseException
-        {
-          try {
-            return new URI(x);
-          } catch (final URISyntaxException e) {
-            throw new JSONParseException(e);
+        new PartialFunctionType<String, URI, JSONParseException>() {
+          @Override
+          public URI call(final String x)
+              throws JSONParseException {
+            try {
+              return new URI(x);
+            } catch (final URISyntaxException e) {
+              throw new JSONParseException(e);
+            }
           }
-        }
-      });
+        });
   }
 
   /**
    * @param key A key assumed to be holding a value
    * @param n   A node
-   *
    * @return A URI value from key {@code key}
-   *
    * @throws JSONParseException On type errors
    */
 
   public static URI getURI(
-    final ObjectNode n,
-    final String key)
-    throws JSONParseException
-  {
+      final ObjectNode n,
+      final String key)
+      throws JSONParseException {
+
     NullCheck.notNull(n);
     NullCheck.notNull(key);
 
@@ -494,19 +642,17 @@ public final class JSONParserUtilities
    * @param key A key assumed to be holding a value
    * @param n   A node
    * @param v   A default value
-   *
    * @return A boolean from key {@code key}, or {@code v} if the key does not
    * exist
-   *
    * @throws JSONParseException On type errors
    */
 
   public static boolean getBooleanDefault(
-    final ObjectNode n,
-    final String key,
-    final boolean v)
-    throws JSONParseException
-  {
+      final ObjectNode n,
+      final String key,
+      final boolean v)
+      throws JSONParseException {
+
     NullCheck.notNull(n);
     NullCheck.notNull(key);
 
@@ -519,17 +665,15 @@ public final class JSONParserUtilities
   /**
    * @param key A key assumed to be holding a value
    * @param n   A node
-   *
    * @return A big integer value from key {@code key}, if the key exists
-   *
    * @throws JSONParseException On type errors
    */
 
   public static OptionType<BigInteger> getBigIntegerOptional(
-    final ObjectNode n,
-    final String key)
-    throws JSONParseException
-  {
+      final ObjectNode n,
+      final String key)
+      throws JSONParseException {
+
     NullCheck.notNull(n);
     NullCheck.notNull(key);
 
@@ -542,17 +686,15 @@ public final class JSONParserUtilities
   /**
    * @param key A key assumed to be holding a value
    * @param n   A node
-   *
    * @return A big integer value from key {@code key}
-   *
    * @throws JSONParseException On type errors
    */
 
   public static BigInteger getBigInteger(
-    final ObjectNode n,
-    final String key)
-    throws JSONParseException
-  {
+      final ObjectNode n,
+      final String key)
+      throws JSONParseException {
+
     NullCheck.notNull(n);
     NullCheck.notNull(key);
 
