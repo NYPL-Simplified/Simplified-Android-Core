cedbb1a8f150f07f79a0b156e6dc4fa318d09306
Mark Raynsford <code@io7m.com>
2019-01-29 12:31:51 +0000

    Add missing device test cases

---
+++ simplified-tests-android/src/androidTest/java/org/nypl/simplified/tests/android/books/book_database/BookDatabaseTest.java
@@ -0,0 +1,30 @@
+package org.nypl.simplified.tests.android.books.book_database;
+
+import android.content.Context;
+import android.support.test.InstrumentationRegistry;
+import android.support.test.filters.MediumTest;
+import android.support.test.runner.AndroidJUnit4;
+
+import org.jetbrains.annotations.NotNull;
+import org.junit.Before;
+import org.junit.runner.RunWith;
+import org.mockito.Mockito;
+import org.nypl.simplified.tests.books.book_database.BookDatabaseContract;
+
+@RunWith(AndroidJUnit4.class)
+@MediumTest
+public final class BookDatabaseTest extends BookDatabaseContract {
+
+  private Context instrumentationContext;
+
+  @Before
+  public void setup() {
+    this.instrumentationContext = InstrumentationRegistry.getContext();
+  }
+
+  @Override
+  protected Context context() {
+    return this.instrumentationContext;
+  }
+
+}
---
+++ simplified-tests-android/src/androidTest/java/org/nypl/simplified/tests/android/books/book_database/BookIDTest.java
@@ -0,0 +1,13 @@
+package org.nypl.simplified.tests.android.books.book_database;
+
+import android.support.test.filters.MediumTest;
+import android.support.test.runner.AndroidJUnit4;
+
+import org.junit.runner.RunWith;
+import org.nypl.simplified.tests.books.book_database.BookIDContract;
+
+@RunWith(AndroidJUnit4.class)
+@MediumTest
+public final class BookIDTest extends BookIDContract {
+
+}
