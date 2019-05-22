3accdf4281056fdd526ac4cf730e769c5be1a395
Mark Raynsford <code@io7m.com>
2019-05-22 18:11:22 +0000

    Try updating Android before creating an AVD

--- .travis-device-pre.sh
+++ .travis-device-pre.sh
@@ -23,35 +23,11 @@ yes | sdkmanager tools \
   >> .travis/device-pre.txt 2>&1 \
   || fatal "could not download avdmanager"
 
-#------------------------------------------------------------------------
-# Download Emulator
-
 info "downloading emulator"
 
 yes | sdkmanager emulator \
   >> .travis/device-pre.txt 2>&1 \
   || fatal "could not download emulator"
-
-#------------------------------------------------------------------------
-# Create the emulator and start it
-
-info "creating an AVD"
-
-echo no | avdmanager create avd \
-  --name test \
-  --force \
-  --package 'system-images;android-21;default;armeabi-v7a' \
-  >> .travis/device-pre.txt 2>&1 \
-  || fatal "could not create AVD"
-
-info "starting an emulator"
-
-$ANDROID_HOME/emulator/emulator -avd test -no-audio -no-window & \
-  >> .travis/device-pre.txt 2>&1 \
-  || fatal "could not start AVD"
-
-#------------------------------------------------------------------------
-# Install SDKs
 
 info "installing platforms"
 
@@ -70,4 +46,22 @@ info "agreeing to licenses"
 yes | sdkmanager --licenses \
   >> .travis/device-pre.txt 2>&1 \
   || fatal "could not agree to licenses"
+
+#------------------------------------------------------------------------
+# Create the emulator and start it
+
+info "creating an AVD"
+
+echo no | avdmanager create avd \
+  --name test \
+  --force \
+  --package 'system-images;android-21;default;armeabi-v7a' \
+  >> .travis/device-pre.txt 2>&1 \
+  || fatal "could not create AVD"
+
+info "starting an emulator"
+
+$ANDROID_HOME/emulator/emulator -avd test -no-audio -no-window & \
+  >> .travis/device-pre.txt 2>&1 \
+  || fatal "could not start AVD"
 
