e3623e5206c438d22a5d64b0179641b12cb24ac0
Mark Raynsford <code@io7m.com>
2019-05-22 17:21:54 +0000

    Added info statements

--- .travis-device-pre.sh
+++ .travis-device-pre.sh
@@ -7,10 +7,17 @@ fatal()
   exit 1
 }
 
+info()
+{
+  echo "info: $1" 1>&2
+}
+
 mkdir -p .travis || fatal "could not create .travis"
 
 #------------------------------------------------------------------------
 # Download avdmanager
+
+info "downloading avdmanager"
 
 yes | sdkmanager tools \
   >> .travis/device-pre.txt 2>&1 \
@@ -19,12 +26,16 @@ yes | sdkmanager tools \
 #------------------------------------------------------------------------
 # Create the emulator and start it
 
+info "creating an AVD"
+
 echo no | avdmanager create avd \
   --name test \
   --force \
   --package 'system-images;android-21;default;armeabi-v7a' \
   >> .travis/device-pre.txt 2>&1 \
   || fatal "could not create AVD"
+
+info "starting an emulator"
 
 emulator -avd test -no-audio -no-window & \
   >> .travis/device-pre.txt 2>&1 \
@@ -33,13 +44,19 @@ emulator -avd test -no-audio -no-window & \
 #------------------------------------------------------------------------
 # Install SDKs
 
+info "installing platforms"
+
 yes | sdkmanager "platforms;android-28" \
   >> .travis/device-pre.txt 2>&1 \
   || fatal "could not install platform"
 
+info "updating platforms"
+
 yes | sdkmanager --update \
   >> .travis/device-pre.txt 2>&1 \
   || fatal "could not update platform"
+
+info "agreeing to licenses"
 
 yes | sdkmanager --licenses \
   >> .travis/device-pre.txt 2>&1 \
