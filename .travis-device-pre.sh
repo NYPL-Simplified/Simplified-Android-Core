bf56ed3a33f5a864f7d69281c6e3399d67a264be
Mark Raynsford <code@io7m.com>
2019-05-22 16:14:42 +0000

    Ensure sdkmanager is installed

--- .travis-device-pre.sh
+++ .travis-device-pre.sh
@@ -2,9 +2,20 @@
 
 echo "info: starting up emulator" 1>&2
 
+#------------------------------------------------------------------------
+# Download avdmanager
+
+yes | sdkmanager tools || exit 1
+
+#------------------------------------------------------------------------
+# Create the emulator and start it
+
 avdmanager list target || exit 1
 avdmanager create avd --name test --force || exit 1
 emulator -avd test -no-audio -no-window &
+
+#------------------------------------------------------------------------
+# Install SDKs
 
 echo "info: installing Android SDKs" 1>&2
 
