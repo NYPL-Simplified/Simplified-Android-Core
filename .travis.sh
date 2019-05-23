ed9ef3f1aca028a8949edee550bf70ae60279eef
Mark Raynsford <code@io7m.com>
2019-05-23 13:24:26 +0000

    Clone Certificates repos in Travis build

--- .travis.sh
+++ .travis.sh
@@ -12,12 +12,24 @@ then
   exit 1
 fi
 
+if [ -z "${NYPL_GITHUB_ACCESS_TOKEN}" ]
+then
+  echo "error: NYPL_GITHUB_ACCESS_TOKEN is not defined" 1>&2
+  exit 1
+fi
+
+#------------------------------------------------------------------------
+# Clone GitHub repos
+
+mkdir -p .travis || exit 1
+
+git clone "https://${NYPL_GITHUB_ACCESS_TOKEN}@www.github.com/NYPL-Simplified/Certificates" ".travis/credentials" || exit 1
+
 (cat <<EOF
 org.librarysimplified.nexus.username=${NYPL_NEXUS_USER}
 org.librarysimplified.nexus.password=${NYPL_NEXUS_PASSWORD}
 
-org.gradle.parallel=true
-org.gradle.jvmargs=-Xms1024m -Xmx2048m
+org.gradle.parallel=false
 org.nypl.simplified.with_findaway=true
 EOF
 ) > gradle.properties.tmp || exit 1
