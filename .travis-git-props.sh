1fc4a1fba9ea4f420b47f5295d255e33c73cd08b
Mark Raynsford <code@io7m.com>
2019-05-28 17:56:13 +0000

    Attempt to push binaries on successful commit

--- .travis-git-props.sh
+++ .travis-git-props.sh
@@ -2,8 +2,10 @@
 
 GP_GIT_COMMIT=$(git rev-list --max-count=1 HEAD) || exit 1
 GP_GIT_BRANCH=$(git rev-parse --abbrev-ref HEAD) || exit 1
+SIMPLYE_VERSION=$(grep versionCode simplified-app-simplye/version.properties | sed 's/versionCode/simplye.versionCode/g') || exit 1
 
 cat <<EOF
-git.commit: ${GP_GIT_COMMIT}
-git.branch: ${GP_GIT_BRANCH}
+git.commit=${GP_GIT_COMMIT}
+git.branch=${GP_GIT_BRANCH}
+${SIMPLYE_VERSION}
 EOF
--- .travis-post.sh
+++ .travis-post.sh
@@ -15,3 +15,40 @@ info()
   echo "info: $1" 1>&2
 }
 
+WORKING_DIRECTORY=$(pwd) ||
+  fatal "could not save working directory"
+
+BINARIES_DIRECTORY=$(realpath .travis/binaries) ||
+  fatal "could not resolve binaries directory"
+
+cd "${BINARIES_DIRECTORY}" ||
+  fatal "could not switch to binaries directory"
+
+git rm -f *.apk ||
+  fatal "could not delete APKs"
+git rm -f build.properties ||
+  fatal "could not delete build properties"
+
+cd "${WORKING_DIRECTORY}" ||
+  fatal "could not restore working directory"
+
+cp -v ./simplified-app-simplye/build/outputs/apk/debug/*   "${BINARIES_DIRECTORY}"
+cp -v ./simplified-app-simplye/build/outputs/apk/release/* "${BINARIES_DIRECTORY}"
+
+./.travis-git-props.sh > "${BINARIES_DIRECTORY}/build.properties" ||
+  fatal "could not save build properties"
+./.travis-git-message.sh > "${WORKING_DIRECTORY}/commit-message.txt" ||
+  fatal "could not save commit message"
+
+cd "${BINARIES_DIRECTORY}" ||
+  fatal "could not switch to binaries directory"
+
+git add *.apk ||
+  fatal "could not add APKs to index"
+git add build.properties ||
+  fatal "could not add build properties to index"
+git commit --file="${WORKING_DIRECTORY}/commit-message.txt" ||
+  fatal "could not commit"
+git push ||
+  fatal "could not push"
+
