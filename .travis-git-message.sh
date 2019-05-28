7d40c404f4ee23343c4f7d8d9b6f6306702d2fa7
Mark Raynsford <code@io7m.com>
2019-05-28 19:37:53 +0000

    Remove branch information, the shallow clone makes it inaccurate

--- .travis-git-message.sh
+++ .travis-git-message.sh
@@ -1,14 +1,12 @@
 #!/bin/sh
 
 GP_GIT_COMMIT=$(git rev-list --max-count=1 HEAD) || exit 1
-GP_GIT_BRANCH=$(git branch | grep '^\*' | awk '{print $NF}') || exit 1
 SIMPLYE_VERSION=$(grep versionCode simplified-app-simplye/version.properties | sed 's/versionCode=//g') || exit 1
 
 cat <<EOF
 Build of ${GP_GIT_COMMIT}
 
 Git commit: ${GP_GIT_COMMIT}
-Git branch: ${GP_GIT_BRANCH}
 Version code: ${SIMPLYE_VERSION}
 
 Commit link: https://www.github.com/NYPL-Simplified/android/commit/${GP_GIT_COMMIT}
--- .travis-git-props.sh
+++ .travis-git-props.sh
@@ -1,11 +1,9 @@
 #!/bin/sh
 
 GP_GIT_COMMIT=$(git rev-list --max-count=1 HEAD) || exit 1
-GP_GIT_BRANCH=$(git branch | grep '^\*' | awk '{print $NF}') || exit 1
 SIMPLYE_VERSION=$(grep versionCode simplified-app-simplye/version.properties | sed 's/versionCode/simplye.versionCode/g') || exit 1
 
 cat <<EOF
 git.commit=${GP_GIT_COMMIT}
-git.branch=${GP_GIT_BRANCH}
 ${SIMPLYE_VERSION}
 EOF
