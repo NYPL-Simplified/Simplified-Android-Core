3f9f7d51b6b19ab793a8b9df35812f95724ade1f
Mark Raynsford <code@io7m.com>
2019-06-11 13:10:10 +0000

    Add DRM support indicator in developer settings
    This adds a UI element in the hidden developer settings section
    that clearly indicates whether or not DRM support is enabled.
    
    It also adds a toggle switch for showing/hiding non-production
    libraries but this switch current does nothing.
    
    Testing: Tested informally in the NYPL collection.
    
    Affects: https://jira.nypl.org/browse/SIMPLY-2010
    Affects: https://jira.nypl.org/browse/SIMPLY-1967
    Affects: https://jira.nypl.org/browse/SIMPLY-1978

--- simplified-app-shared/src/main/java/org/nypl/simplified/app/settings/SettingsVersionActivity.kt
+++ simplified-app-shared/src/main/java/org/nypl/simplified/app/settings/SettingsVersionActivity.kt
@@ -2,25 +2,33 @@ package org.nypl.simplified.app.settings
 
 import android.content.pm.PackageManager
 import android.os.Bundle
+import android.support.v4.content.ContextCompat
 import android.view.MenuItem
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import android.widget.TableLayout
+import android.widget.TableRow
 import android.widget.TextView
+import org.nypl.simplified.app.AdobeDRMServices
 import org.nypl.simplified.app.BuildConfig
 import org.nypl.simplified.app.R
 import org.nypl.simplified.app.Simplified
 import org.nypl.simplified.app.profiles.ProfileTimeOutActivity
+import org.slf4j.LoggerFactory
 
 class SettingsVersionActivity : ProfileTimeOutActivity() {
 
-  private var buildClicks = 0
+  private var buildClicks = 1
   private lateinit var crashButton: Button
+  private lateinit var drmTable: TableLayout
   private lateinit var versionText: TextView
   private lateinit var versionTitle: TextView
   private lateinit var buildTitle: TextView
   private lateinit var buildText: TextView
   private lateinit var developerOptions: ViewGroup
+
+  private val logger = LoggerFactory.getLogger(SettingsVersionActivity::class.java)
 
   override fun onCreate(state: Bundle?) {
     super.onCreate(state)
@@ -41,17 +49,8 @@ class SettingsVersionActivity : ProfileTimeOutActivity() {
       this.findViewById(R.id.settings_version_version_title)
     this.versionText =
       this.findViewById(R.id.settings_version_version)
-
-    this.buildTitle.setOnClickListener {
-      if (this.buildClicks >= 7) {
-        this.developerOptions.visibility = View.VISIBLE
-      }
-      ++this.buildClicks
-    }
-
-    this.crashButton.setOnClickListener {
-      throw OutOfMemoryError("Pretending to have run out of memory!")
-    }
+    this.drmTable =
+      this.findViewById(R.id.settings_version_drm_support)
   }
 
   override fun onOptionsItemSelected(item: MenuItem): Boolean {
@@ -75,6 +74,17 @@ class SettingsVersionActivity : ProfileTimeOutActivity() {
       bar.setHomeButtonEnabled(false)
     }
 
+    this.buildTitle.setOnClickListener {
+      if (this.buildClicks >= 7) {
+        this.developerOptions.visibility = View.VISIBLE
+      }
+      ++this.buildClicks
+    }
+
+    this.crashButton.setOnClickListener {
+      throw OutOfMemoryError("Pretending to have run out of memory!")
+    }
+
     try {
       val pkgManager = this.getPackageManager()
       val pkgInfo = pkgManager.getPackageInfo(packageName, 0)
@@ -85,5 +95,30 @@ class SettingsVersionActivity : ProfileTimeOutActivity() {
 
     this.developerOptions.visibility = View.GONE
     this.buildText.text = BuildConfig.GIT_COMMIT
+
+    this.drmTable.removeAllViews()
+    this.drmTable.addView(drmACSSupportRow())
+  }
+
+  private fun drmACSSupportRow(): TableRow {
+    val row =
+      this.layoutInflater.inflate(
+        R.layout.settings_version_table_item, this.drmTable, false) as TableRow
+    val key =
+      row.findViewById<TextView>(R.id.key)
+    val value =
+      row.findViewById<TextView>(R.id.value)
+
+    key.text = "Adobe ACS"
+    value.text = try {
+      AdobeDRMServices.newAdobeDRM(this, AdobeDRMServices.getPackageOverride(this.resources))
+      value.setTextColor(ContextCompat.getColor(this, R.color.simplified_material_green_primary))
+      "Supported"
+    } catch (e: Throwable) {
+      this.logger.debug("DRM unsupported: ", e)
+      value.setTextColor(ContextCompat.getColor(this, R.color.simplified_material_red_primary))
+      "Unsupported"
+    }
+    return row
   }
 }
