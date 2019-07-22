1755003a423329f723d82a861d5d4ce72b9efe94
Matt Niehoff <matt.j.niehoff@gmail.com>
2019-07-22 15:50:40 -0500

    End of day commit.
    Updates to use new profile events introduced in 6ac273512023840fe5aa8b1e7a2fe6d89e45150e
    
    Refactors function names and adds descriptions to most.
    
    Need to do testing to ensure this is working, I don't think this is quite done yet.

--- simplified-app-shared/src/main/java/org/nypl/simplified/app/services/NotificationsService.kt
+++ simplified-app-shared/src/main/java/org/nypl/simplified/app/services/NotificationsService.kt
@@ -12,11 +12,11 @@ import org.nypl.simplified.books.api.BookEvent
 import org.nypl.simplified.books.api.BookID
 import org.nypl.simplified.books.book_registry.BookRegistryReadableType
 import org.nypl.simplified.books.book_registry.BookStatusEvent
+import org.nypl.simplified.books.book_registry.BookStatusHeld
 import org.nypl.simplified.books.book_registry.BookWithStatus
 import org.nypl.simplified.observable.ObservableReadableType
-import org.nypl.simplified.profiles.api.ProfileAccountSelectEvent
 import org.nypl.simplified.profiles.api.ProfileEvent
-import org.nypl.simplified.profiles.api.ProfileSelected
+import org.nypl.simplified.profiles.api.ProfileSelection
 import org.nypl.simplified.threads.NamedThreadPools
 import org.slf4j.LoggerFactory
 import java.util.concurrent.ExecutorService
@@ -37,76 +37,156 @@ class NotificationsService(
 
     val profileSubscription =
             profileEvents.subscribe(this::onProfileEvent)
-    val bookRegistrySubscription =
+    var bookRegistrySubscription =
             bookRegistry.bookEvents().subscribe(this::onBookEvent)
 
-    /*
-    When changing from SimplyE to NYPL, the ProfileAccountSelectSucceededEvent is firing
+    var registryCache: Map<BookID, BookWithStatus> = mapOf()
+
+
+    /**
+     * Method for handling [ProfileEvent]s the service is subscribed to.
      */
     private fun onProfileEvent(event: ProfileEvent) {
         logger.debug("NotificationsService::onProfileEvent $event")
         executor.execute {
-            if (event is ProfileSelected ||
-                    event is ProfileAccountSelectEvent.ProfileAccountSelectSucceeded) {
-                publishNotification("onProfileEvent", event.toString())
-                getBookStatuses()
+            if (event is ProfileSelection.ProfileSelectionInProgress) {
+                onProfileSelectionInProgress()
+            } else if (event is ProfileSelection.ProfileSelectionCompleted) {
+                onProfileSelectionCompleted()
             }
         }
     }
 
-    /*
+    /**
+     * Defines logic for handling [ProfileEvent] [ProfileSelection.ProfileSelectionInProgress].
+     * Unsubscribes from [BookEvent]s and clears the cached bookRegistry.
+     * Once the profile selection is completed, [onProfileSelectionCompleted] will
+     * handle resubscribing for changes.
+     */
+    private fun onProfileSelectionInProgress() {
+        logger.debug("NotificationsService::onProfileSelectionInProgress")
+        unsubscribeFromBookEvents()
+        clearBookRegistryCache()
+    }
 
+    /**
+     * Defines logic for handling [ProfileEvent] [ProfileSelection.ProfileSelectionCompleted].
+     * Caches the bookRegistry and subscribes to [BookEvent]s.
+     * When the [ProfileEvent] [ProfileSelection.ProfileSelectionCompleted] fires,
+     * the registry will represent what is currently on disc.
+     */
+    private fun onProfileSelectionCompleted() {
+        logger.debug("NotificationsService::onProfileSelectionCompleted")
+        cacheBookRegistry()
+        subscribeToBookEvents()
+    }
+
+    /**
+     * Method for handling [BookEvent]s the service is subscribed to.
      */
     private fun onBookEvent(event: BookEvent) {
-        logger.debug("NotificationsService:: onBookEvent $event")
+        logger.debug("NotificationsService::onBookEvent $event")
         executor.execute {
             if (event is BookStatusEvent) {
                 /*
-                   BookStatusEvent is only giving me BOOK_CHANGED and BOOK_REMOVED.
-                   While a book Download goes on, this fires repeatedly as the download
-                   progress get updated.
+                   When a BookStatusEvent with Type BOOK_CHANGED, we want to compare
+                   the current registry's status with the cached version.
                  */
-
-                publishNotification("onBookEvent", event.type().toString())
-                // getBookStatuses()
+                if (event.type() == BookStatusEvent.Type.BOOK_CHANGED) {
+                    val bookStatus = (bookRegistry.book(event.book()) as Some<BookWithStatus>).get()
+                    compareToCache(bookStatus)
+                }
             }
         }
     }
 
-    private fun getBookStatuses() {
-        val statusesBefore: Map<BookID, BookWithStatus> = mapOf()
-        val statusesNow = getBookStatusesFromRegistry()
-        for (book in statusesBefore) {
-            var bookId = book.key
-            val statusBefore = statusesBefore[bookId]
-            val statusNow = statusesNow[bookId]
-
-            // Do any comparison on statuses here
-            if (statusChangedSufficiently(statusBefore, statusNow)) {
-//                publishNotification(...)
-            }
+    private fun compareToCache(bookStatus: BookWithStatus?) {
+        // TODO: Validate this behavior.
+        var cachedBookStatus = registryCache[bookStatus?.book()?.id]
+        if (statusChangedSufficiently(bookStatus, cachedBookStatus)) {
+            publishNotification("Hold Ready", "One or more book holds are available for checkout")
         }
-
-        //saveStatuses(statusesNow)
     }
+
+    /**
+     * Caches the BookRegistry.
+     * This is used to check against [BookStatusEvent]s if a new book
+     * is in a status we want to take action on (i.e. show notification)
+     */
+    private fun cacheBookRegistry() {
+        logger.debug("In NotificationsService::cacheBookRegistry")
+        registryCache = getBookStatusesFromRegistry()
+    }
+
+    /**
+     * Resets the cached BookRegistry.
+     */
+    private fun clearBookRegistryCache() {
+        logger.debug("In NotificationsService::clearBookRegistryCache")
+        registryCache = mapOf()
+    }
+
+    /**
+     * Subscribes [NotificationsService] to receive [BookStatusEvent]s
+     */
+    private fun subscribeToBookEvents() {
+        logger.debug("NotificationsService::subscribeToBookEvents")
+        bookRegistrySubscription =
+                bookRegistry.bookEvents().subscribe(this::onBookEvent)
+    }
+
+    /**
+     * Unsubscribes [NotificationsService] from [BookStatusEvent]s
+     */
+    private fun unsubscribeFromBookEvents() {
+        logger.debug("NotificationsService::unsubscribeFromBookEvents")
+        bookRegistrySubscription.unsubscribe()
+    }
+
+//    private fun getBookStatuses() {
+//        val statusesBefore: Map<BookID, BookWithStatus> = mapOf()
+//        val statusesNow = getBookStatusesFromRegistry()
+//        for (book in statusesBefore) {
+//            var bookId = book.key
+//            val statusBefore = statusesBefore[bookId]
+//            val statusNow = statusesNow[bookId]
+//
+//            // Do any comparison on statuses here
+//            if (statusChangedSufficiently(statusBefore, statusNow)) {
+////                publishNotification(...)
+//            }
+//        }
+//    }
 
     private fun statusChangedSufficiently(statusBefore: BookWithStatus?, statusNow: BookWithStatus?): Boolean {
+        logger.debug("NotificationsService::statusChangedSufficiently comparing $statusBefore to $statusNow")
+        if (statusBefore != null) {
+            return statusBefore.status() is BookStatusHeld
+        }
+
+        // No status change met criteria
         return false
     }
 
+    /**
+     *
+     */
     private fun getBookStatusesFromRegistry(): Map<BookID, BookWithStatus> {
+        logger.debug("NotificationsService::getBookStatusesFromRegistry")
         var books = bookRegistry.books()
-        var statuses: MutableMap<BookID, BookWithStatus> = mutableMapOf()
+//        var statuses: MutableMap<BookID, BookWithStatus> = mutableMapOf()
+//
+//        for (book in books) {
+//            val bookId = book.key
+//            statuses[bookId] = (bookRegistry.book(bookId) as Some<BookWithStatus>).get()
+//        }
 
-        for (book in books) {
-            val bookId = book.key
-            statuses[bookId] = (bookRegistry.book(bookId) as Some<BookWithStatus>).get()
-        }
-
-        return statuses.toMap()
+        return bookRegistry.books().toMap()
     }
 
     private fun publishNotification(notificationTitle: String, notificationContent: String) {
+        logger.debug("NotificationsService::publishNotification " +
+                "with title $notificationTitle and content $notificationContent")
         val notificationManager = context.getSystemService(NOTIFICATION_SERVICE) as NotificationManager
 
         createNotificationChannel(notificationManager, "Channel Name", "Channel Description")
@@ -124,7 +204,8 @@ class NotificationsService(
     private fun createNotificationChannel(notificationManager: NotificationManager,
                                           channelName: String,
                                           channelDescription: String) {
-        logger.debug("createNotificationChannel")
+        logger.debug("NotificationsService::createNotificationChannel " +
+                "with channel name $channelName and description $channelDescription")
 
         // Create the NotificationChannel, but only on API 26+ because
         // the NotificationChannel class is new and not in the support library
