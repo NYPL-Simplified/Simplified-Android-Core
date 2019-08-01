48294116cde4d0ce3dc1d5fdd8bdb5ae3b63c6a2
Matt Niehoff <matt.j.niehoff@gmail.com>
2019-08-01 09:53:09 -0500

    Builds out test class.
    I'm failing on setting the Book Registry subscription when I send the Profile Event.
    
    Debugging, I'm hitting it, but it never steps into the `executor` code.

---
+++ simplified-tests/src/test/java/org/nypl/simplified/tests/local/notifications/NotificationsServiceTest.java
@@ -0,0 +1,7 @@
+package org.nypl.simplified.tests.local.notifications;
+
+import org.nypl.simplified.tests.notifications.NotificationsServiceContract;
+
+public final class NotificationsServiceTest extends NotificationsServiceContract {
+
+}
