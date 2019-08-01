43fda92fd5fa425545c347e76ddcb4e33a491309
Matt Niehoff <matt.j.niehoff@gmail.com>
2019-08-01 09:53:09 -0500

    Builds out test class.
    I'm failing on setting the Book Registry subscription when I send the Profile Event.
    
    Debugging, I'm hitting it, but it never steps into the `executor` code.

---
+++ simplified-tests/src/main/java/org/nypl/simplified/tests/notifications/NotificationsServiceContract.kt
@@ -0,0 +1,96 @@
+package org.nypl.simplified.tests.notifications
+
+import android.content.Context
+import junit.framework.Assert
+import org.junit.Before
+import org.junit.Test
+import org.mockito.Mockito
+import org.nypl.simplified.accounts.api.AccountID
+import org.nypl.simplified.books.book_registry.BookRegistry
+import org.nypl.simplified.books.book_registry.BookRegistryType
+import org.nypl.simplified.notifications.NotificationResourcesType
+import org.nypl.simplified.notifications.NotificationsService
+import org.nypl.simplified.observable.Observable
+import org.nypl.simplified.observable.ObservableType
+import org.nypl.simplified.profiles.api.ProfileAccountSelectEvent
+import org.nypl.simplified.profiles.api.ProfileEvent
+import org.nypl.simplified.tests.R
+import org.nypl.simplified.tests.books.book_database.BookDatabaseContract
+import org.slf4j.LoggerFactory
+import java.util.UUID
+
+abstract class NotificationsServiceContract {
+    private val logger =
+            LoggerFactory.getLogger(BookDatabaseContract::class.java)
+
+    private lateinit var context: Context
+    private lateinit var profileEvents: ObservableType<ProfileEvent>
+    private lateinit var bookRegistry: BookRegistryType
+    private lateinit var notificationsService: NotificationsService
+
+    @Before
+    fun setUp() {
+        this.bookRegistry = BookRegistry.create()
+        this.profileEvents = Observable.create<ProfileEvent>()
+        this.context = Mockito.mock(Context::class.java)
+
+        val notificationResourcesType = object : NotificationResourcesType {
+            override val intentClass: Class<*>
+                get() = String::class.java
+            override val titleReadyNotificationContent: String
+                get() = "notification content"
+            override val titleReadyNotificationTitle: String
+                get() = "notification title"
+            override val smallIcon: Int
+                get() = R.drawable.simplified_button
+        }
+
+        this.notificationsService = NotificationsService(context, profileEvents, bookRegistry, notificationResourcesType)
+    }
+
+    @Test
+    fun testInitializeNotificationsService() {
+        // Instance created and properties assigned
+        Assert.assertNotNull(notificationsService)
+
+        Assert.assertEquals(profileEvents, notificationsService.profileEvents)
+        Assert.assertEquals(bookRegistry, notificationsService.bookRegistry)
+        Assert.assertEquals(context, notificationsService.context)
+
+        // No book subscription on initialization
+        Assert.assertNull(notificationsService.bookRegistrySubscription)
+    }
+
+    @Test
+    fun testOnProfileSelectionCompletedSetsBookRegistrySubscription() {
+        this.profileEvents.send(ProfileAccountSelectEvent.ProfileAccountSelectSucceeded.of(AccountID(UUID.randomUUID()), AccountID(UUID.randomUUID())))
+        Assert.assertNotNull(notificationsService.bookRegistrySubscription)
+    }
+
+    @Test
+    fun testOnProfileSelectionCompletedSetBookRegistryCache() {
+        Assert.fail()
+    }
+
+    @Test
+    fun testOnProfileSelectionInProgressUnsubscribeBookEvents() {
+        Assert.fail()
+    }
+
+    @Test
+    fun testOnProfileSelectionInProgressClearsBookRegistryCache() {
+        Assert.fail()
+    }
+
+    @Test
+    fun testNoNotificationOnHeldReadyToHeldReadyStatus() {
+        // If the book in the registry is already HeldReady we don't need to show the notification
+        Assert.fail()
+    }
+
+    @Test
+    fun testShowNotificationOnHeldReadyStatusChange() {
+        // Show notification if new status is HeldReady and cache is Held
+        Assert.fail()
+    }
+}
