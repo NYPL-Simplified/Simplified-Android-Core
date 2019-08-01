4b9b9da8f9bcd16e199faf287841af3955b479ed
Mark Raynsford <code@io7m.com>
2019-08-01 17:07:30 +0000

    Add an example test

--- simplified-tests/src/main/java/org/nypl/simplified/tests/notifications/NotificationsServiceContract.kt
+++ simplified-tests/src/main/java/org/nypl/simplified/tests/notifications/NotificationsServiceContract.kt
@@ -1,6 +1,7 @@
 package org.nypl.simplified.tests.notifications
 
 import android.content.Context
+import com.io7m.jfunctional.ProcedureType
 import junit.framework.Assert
 import org.junit.Before
 import org.junit.Test
@@ -8,16 +9,22 @@ import org.mockito.Mockito
 import org.nypl.simplified.accounts.api.AccountID
 import org.nypl.simplified.books.book_registry.BookRegistry
 import org.nypl.simplified.books.book_registry.BookRegistryType
+import org.nypl.simplified.books.book_registry.BookStatusEvent
 import org.nypl.simplified.notifications.NotificationResourcesType
 import org.nypl.simplified.notifications.NotificationsService
 import org.nypl.simplified.observable.Observable
+import org.nypl.simplified.observable.ObservableReadableType
+import org.nypl.simplified.observable.ObservableSubscriptionType
 import org.nypl.simplified.observable.ObservableType
 import org.nypl.simplified.profiles.api.ProfileAccountSelectEvent
 import org.nypl.simplified.profiles.api.ProfileEvent
+import org.nypl.simplified.profiles.api.ProfileID
+import org.nypl.simplified.profiles.api.ProfileSelection
 import org.nypl.simplified.tests.R
 import org.nypl.simplified.tests.books.book_database.BookDatabaseContract
 import org.slf4j.LoggerFactory
 import java.util.UUID
+import java.util.concurrent.CountDownLatch
 import java.util.concurrent.ThreadFactory
 
 abstract class NotificationsServiceContract {
@@ -29,6 +36,7 @@ abstract class NotificationsServiceContract {
     private lateinit var bookRegistry: BookRegistryType
     private lateinit var notificationsService: NotificationsService
     private lateinit var threadFactory: ThreadFactory
+    private lateinit var notificationResources: NotificationResources
 
     @Before
     fun setUp() {
@@ -36,19 +44,20 @@ abstract class NotificationsServiceContract {
         this.profileEvents = Observable.create<ProfileEvent>()
         this.context = Mockito.mock(Context::class.java)
         this.threadFactory = ThreadFactory { Thread(it) }
+        this.notificationResources = NotificationResources()
 
-        val notificationResourcesType = object : NotificationResourcesType {
-            override val intentClass: Class<*>
-                get() = String::class.java
-            override val titleReadyNotificationContent: String
-                get() = "notification content"
-            override val titleReadyNotificationTitle: String
-                get() = "notification title"
-            override val smallIcon: Int
-                get() = R.drawable.simplified_button
-        }
+        this.notificationsService = NotificationsService(context, this.threadFactory, profileEvents, bookRegistry, this.notificationResources)
+    }
 
-        this.notificationsService = NotificationsService(context, this.threadFactory, profileEvents, bookRegistry, notificationResourcesType)
+    class NotificationResources : NotificationResourcesType {
+        override val intentClass: Class<*>
+            get() = String::class.java
+        override val titleReadyNotificationContent: String
+            get() = "notification content"
+        override val titleReadyNotificationTitle: String
+            get() = "notification title"
+        override val smallIcon: Int
+            get() = R.drawable.simplified_button
     }
 
     @Test
@@ -64,10 +73,51 @@ abstract class NotificationsServiceContract {
         Assert.assertNull(notificationsService.bookRegistrySubscription)
     }
 
-    @Test
+    @Test(timeout = 3_000L)
     fun testOnProfileSelectionCompletedSetsBookRegistrySubscription() {
-        this.profileEvents.send(ProfileAccountSelectEvent.ProfileAccountSelectSucceeded.of(AccountID(UUID.randomUUID()), AccountID(UUID.randomUUID())))
-        Assert.assertNotNull(notificationsService.bookRegistrySubscription)
+
+        /*
+         * Create a countdown latch that only accepts one count down.
+         */
+
+        val subscriptionLatch = CountDownLatch(1)
+
+        /*
+         * Create a fake observable that counts down the above latch each time someone
+         * subscribes to it.
+         */
+
+        val mockSubscription = ObservableSubscriptionType<BookStatusEvent> {
+
+        }
+
+        val mockObservable = object: ObservableReadableType<BookStatusEvent> {
+            override fun subscribe(receiver: ProcedureType<BookStatusEvent>?): ObservableSubscriptionType<BookStatusEvent> {
+                subscriptionLatch.countDown()
+                return mockSubscription
+            }
+
+            override fun count(): Int {
+                return 0
+            }
+        }
+
+        val mockRegistry =
+          Mockito.mock(BookRegistryType::class.java)
+
+        Mockito.`when`(mockRegistry.bookEvents())
+          .thenReturn(mockObservable)
+
+        this.notificationsService =
+          NotificationsService(context, this.threadFactory, profileEvents, mockRegistry, this.notificationResources)
+
+        this.profileEvents.send(ProfileSelection.ProfileSelectionCompleted(ProfileID(UUID.randomUUID())))
+
+        /*
+         * Wait for the subscription latch to count down.
+         */
+
+        subscriptionLatch.await()
     }
 
     @Test
