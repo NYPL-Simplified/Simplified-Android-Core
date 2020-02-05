d72f1b0775ac9cc88f9d662ff68b9a040ec5854f
Mark Raynsford <code@io7m.com>
2020-02-05 16:48:24 +0000

    Use different heap settings for Travis CI
    This configures Gradle to use a different set of heap settings. These
    settings have reduced build times on two similarly structured projects
    to ~25% of their original times.

--- .travis-build.sh
+++ .travis-build.sh
@@ -1,10 +1,21 @@
 #!/bin/sh
 
 #------------------------------------------------------------------------
+# Configure some Gradle settings that work better in CI containers
+
+cat <<EOF
+
+org.gradle.daemon=true
+org.gradle.configureondemand=true
+org.gradle.jvmargs=-Xmx4g -XX:MaxPermSize=2048m -XX:+HeapDumpOnOutOfMemoryError
+
+EOF >> gradle.properties
+
+#------------------------------------------------------------------------
 # Execute the build
 
 ./gradlew clean test assemble \
   -Porg.librarysimplified.keyAlias=nypl \
   -Porg.librarysimplified.keyPassword=${NYPL_SIGNING_KEY_PASSWORD} \
-  -Porg.librarysimplified.storePassword=${NYPL_SIGNING_STORE_PASSWORD} \
-  -Porg.gradle.parallel=false
+  -Porg.librarysimplified.storePassword=${NYPL_SIGNING_STORE_PASSWORD}
+
