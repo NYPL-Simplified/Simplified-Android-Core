c03e442ec80e6652fff53b189164ba7ec2150f78
Mark Raynsford <code@io7m.com>
2020-02-05 17:07:47 +0000

    Use "exec" to avoid having a shell process sitting around

--- .travis-build.sh
+++ .travis-build.sh
@@ -3,7 +3,7 @@
 #------------------------------------------------------------------------
 # Execute the build
 
-./gradlew clean test assemble \
+exec ./gradlew clean test assemble \
   -Porg.librarysimplified.keyAlias=nypl \
   -Porg.librarysimplified.keyPassword=${NYPL_SIGNING_KEY_PASSWORD} \
   -Porg.librarysimplified.storePassword=${NYPL_SIGNING_STORE_PASSWORD} \
