fc4c2e93ff17de1d69cb805d9cbb240b3e9912b2
Mark Raynsford <code@io7m.com>
2020-02-05 17:00:32 +0000

    Use the -P flag to pass properties to Gradle
    This switches to using the -P flag instead of appending properties
    to the gradle.properties file.

--- .travis-build.sh
+++ .travis-build.sh
@@ -1,21 +1,12 @@
 #!/bin/sh
 
 #------------------------------------------------------------------------
-# Configure some Gradle settings that work better in CI containers
-
-cat <<EOF
-
-org.gradle.daemon=true
-org.gradle.configureondemand=true
-org.gradle.jvmargs=-Xmx4g -XX:MaxPermSize=2048m -XX:+HeapDumpOnOutOfMemoryError
-
-EOF >> gradle.properties
-
-#------------------------------------------------------------------------
 # Execute the build
 
 ./gradlew clean test assemble \
   -Porg.librarysimplified.keyAlias=nypl \
   -Porg.librarysimplified.keyPassword=${NYPL_SIGNING_KEY_PASSWORD} \
-  -Porg.librarysimplified.storePassword=${NYPL_SIGNING_STORE_PASSWORD}
+  -Porg.librarysimplified.storePassword=${NYPL_SIGNING_STORE_PASSWORD} \
+  -Porg.gradle.configureondemand=true \
+  -Porg.gradle.jvmargs="-Xmx4g -XX:MaxPermSize=2048m -XX:+HeapDumpOnOutOfMemoryError"
 
