7ca36adad12b058ba02c51e00e9e92e57fc56a26
Mark Raynsford <code@io7m.com>
2020-02-05 17:15:05 +0000

    Restore build flags to normal, in an attempt to isolate a Travis CI failure

--- .travis-build.sh
+++ .travis-build.sh
@@ -7,7 +7,5 @@ exec ./gradlew clean test assemble \
   -Porg.librarysimplified.keyAlias=nypl \
   -Porg.librarysimplified.keyPassword=${NYPL_SIGNING_KEY_PASSWORD} \
   -Porg.librarysimplified.storePassword=${NYPL_SIGNING_STORE_PASSWORD} \
-  -Porg.gradle.configureondemand=true \
-  -Porg.gradle.jvmargs="-Xmx2048m -XX:MaxPermSize=512m -XX:+HeapDumpOnOutOfMemoryError" \
   -Porg.gradle.parallel=false
 
