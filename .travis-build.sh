1d7a754a3f84615cbf61a90bec13b835430e4231
Mark Raynsford <code@io7m.com>
2020-02-06 10:20:21 +0000

    Use org.gradle.configureondemand to speed up Travis CI builds

--- .travis-build.sh
+++ .travis-build.sh
@@ -7,4 +7,6 @@
   -Porg.librarysimplified.keyAlias=nypl \
   -Porg.librarysimplified.keyPassword=${NYPL_SIGNING_KEY_PASSWORD} \
   -Porg.librarysimplified.storePassword=${NYPL_SIGNING_STORE_PASSWORD} \
-  -Porg.gradle.parallel=false
+  -Porg.gradle.parallel=false \
+  -Porg.gradle.configureondemand=true
+
