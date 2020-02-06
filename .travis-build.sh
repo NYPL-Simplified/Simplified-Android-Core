0d542c8db5fbf5b6600339a384674e578487a4bb
Mark Raynsford <code@io7m.com>
2020-02-06 10:51:44 +0000

    Use org.gradle.parallel=true to attempt to speed up Travis CI builds

--- .travis-build.sh
+++ .travis-build.sh
@@ -7,7 +7,7 @@
   -Porg.librarysimplified.keyAlias=nypl \
   -Porg.librarysimplified.keyPassword=${NYPL_SIGNING_KEY_PASSWORD} \
   -Porg.librarysimplified.storePassword=${NYPL_SIGNING_STORE_PASSWORD} \
-  -Porg.gradle.parallel=false \
+  -Porg.gradle.parallel=true \
   -Porg.gradle.configureondemand=true \
   -Porg.gradle.jvmargs="-Xmx4g -XX:MaxPermSize=2048m -XX:+HeapDumpOnOutOfMemoryError"
 
