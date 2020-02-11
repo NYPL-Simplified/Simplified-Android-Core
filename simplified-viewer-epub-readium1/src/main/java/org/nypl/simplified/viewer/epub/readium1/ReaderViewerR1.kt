f806899022239aa68b12914827fc4d4561b586b0
Tristan Waddington <tristan.waddington@gmail.com>
2020-02-11 00:16:43 -0800

    Added basic Readium2 viewer provider.
    Books lacking AdobeDRM open with Readium2 viewer; books with AdobeDRM
    open with Readium1 viewer.

--- simplified-viewer-epub-readium1/src/main/java/org/nypl/simplified/viewer/epub/readium1/ReaderViewerR1.kt
+++ simplified-viewer-epub-readium1/src/main/java/org/nypl/simplified/viewer/epub/readium1/ReaderViewerR1.kt
@@ -5,12 +5,8 @@ import org.nypl.simplified.books.api.Book
 import org.nypl.simplified.books.api.BookFormat
 import org.nypl.simplified.feeds.api.FeedEntry
 import org.nypl.simplified.viewer.spi.ViewerProviderType
-import org.slf4j.LoggerFactory
 
 class ReaderViewerR1 : ViewerProviderType {
-
-  private val logger =
-    LoggerFactory.getLogger(ReaderViewerR1::class.java)
 
   override val name: String =
     "org.nypl.simplified.viewer.epub.readium1.ReaderViewerR1"
@@ -20,14 +16,11 @@ class ReaderViewerR1 : ViewerProviderType {
     format: BookFormat
   ): Boolean {
     return when (format) {
+      is BookFormat.BookFormatAudioBook -> false
       is BookFormat.BookFormatEPUB -> {
-        true
+        format.adobeRights != null
       }
-      is BookFormat.BookFormatAudioBook,
-      is BookFormat.BookFormatPDF -> {
-        this.logger.debug("R1 can only open EPUB files")
-        false
-      }
+      is BookFormat.BookFormatPDF -> false
     }
   }
 
