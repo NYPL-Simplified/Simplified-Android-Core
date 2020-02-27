dd6f7ee6fc066e0cc26e18d3f1a13949a490b008
Tristan Waddington <tristan.waddington@gmail.com>
2020-02-26 23:46:04 -0800

    SIMPLY-2567: Add Toolbar to readium2 viewer activity.

--- simplified-viewer-epub-readium2/src/main/java/org/nypl/simplified/viewer/epub/readium2/ReaderActivity.kt
+++ simplified-viewer-epub-readium2/src/main/java/org/nypl/simplified/viewer/epub/readium2/ReaderActivity.kt
@@ -3,6 +3,9 @@ package org.nypl.simplified.viewer.epub.readium2
 import android.app.Activity
 import android.content.Intent
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.view.View
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.google.common.util.concurrent.ListeningExecutorService
@@ -22,15 +25,20 @@ import org.nypl.simplified.feeds.api.FeedEntry
 import java.io.File
 import java.util.concurrent.Executors
 
-/** Stub */
+/**
+ * The main reader activity for reading an EPUB using Readium 2.
+ */
 class ReaderActivity : AppCompatActivity(), SR2ControllerHostType {
-  companion object {
 
+  companion object {
     private const val ARG_BOOK_ID = "org.nypl.simplified.app.ReaderActivity.book"
     private const val ARG_FILE = "org.nypl.simplified.app.ReaderActivity.file"
     private const val ARG_ENTRY = "org.nypl.simplified.app.ReaderActivity.entry"
+    private const val SYSTEM_UI_DELAY_MILLIS = 5000L
 
-    /** Stub */
+    /**
+     * Start a new reader for the given book.
+     */
     fun startActivity(
       context: Activity,
       bookId: BookID,
@@ -50,10 +58,7 @@ class ReaderActivity : AppCompatActivity(), SR2ControllerHostType {
     }
   }
 
-
-  private var controllerSubscription: Disposable? = null
-  private var controller: SR2ControllerType? = null
-
+  private val handler = Handler(Looper.getMainLooper())
   private val ioExecutor =
     MoreExecutors.listeningDecorator(Executors.newFixedThreadPool(1) { runnable ->
       val thread = Thread(runnable)
@@ -61,13 +66,21 @@ class ReaderActivity : AppCompatActivity(), SR2ControllerHostType {
       thread
     })
 
+  private var controllerSubscription: Disposable? = null
+  private var controller: SR2ControllerType? = null
 
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
 
     val file = intent?.extras?.getSerializable(ARG_FILE) as File
+    val entry = intent?.extras?.getSerializable(ARG_ENTRY) as FeedEntry.FeedEntryOPDS
 
     if (savedInstanceState == null) {
+      supportActionBar?.apply {
+        title = entry.feedEntry.title
+        setDisplayHomeAsUpEnabled(true)
+      }
+
       val fragment = SR2ReaderFragment.create(
         SR2ReaderFragmentParameters(
           bookFile = file
@@ -77,9 +90,13 @@ class ReaderActivity : AppCompatActivity(), SR2ControllerHostType {
       supportFragmentManager
         .beginTransaction()
         .replace(android.R.id.content, fragment)
-        .addToBackStack(null)
         .commit()
     }
+  }
+
+  override fun onResumeFragments() {
+    super.onResumeFragments()
+    hideSystemUiDelayed()
   }
 
   override fun onStop() {
@@ -117,6 +134,11 @@ class ReaderActivity : AppCompatActivity(), SR2ControllerHostType {
     TODO("not implemented")
   }
 
+  private fun hideSystemUiDelayed() {
+    handler.removeCallbacksAndMessages(null)
+    handler.postDelayed({ hideSystemUi() }, SYSTEM_UI_DELAY_MILLIS)
+  }
+
   private fun onControllerEvent(event: SR2Event) {
     when (event) {
       is SR2Event.SR2Error.SR2ChapterNonexistent -> {
@@ -131,6 +153,13 @@ class ReaderActivity : AppCompatActivity(), SR2ControllerHostType {
         }
       }
 
+      is SR2Event.SR2OnCenterTapped -> {
+        UIThread.runOnUIThread {
+          toggleSystemUi()
+          hideSystemUiDelayed()
+        }
+      }
+
       is SR2Event.SR2ReadingPositionChanged -> {
         UIThread.runOnUIThread {
           val percent = event.progress * 100.0
@@ -139,5 +168,40 @@ class ReaderActivity : AppCompatActivity(), SR2ControllerHostType {
         }
       }
     }
+  }
+}
+
+/** Returns `true` if fullscreen or immersive mode is not set. */
+private fun Activity.isSystemUiVisible(): Boolean {
+  return window.decorView.systemUiVisibility and View.SYSTEM_UI_FLAG_FULLSCREEN == 0
+}
+
+/** Enable fullscreen or immersive mode. */
+private fun Activity.hideSystemUi() {
+  window.decorView.systemUiVisibility = (
+    View.SYSTEM_UI_FLAG_IMMERSIVE
+      or View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+      or View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+      or View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+      or View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+      or View.SYSTEM_UI_FLAG_FULLSCREEN
+    )
+}
+
+/** Disable fullscreen or immersive mode. */
+private fun Activity.showSystemUi() {
+  window.decorView.systemUiVisibility = (
+    View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+      or View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+      or View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+    )
+}
+
+/** Toggle fullscreen or immersive mode. */
+private fun Activity.toggleSystemUi() {
+  if (isSystemUiVisible()) {
+    hideSystemUi()
+  } else {
+    showSystemUi()
   }
 }
