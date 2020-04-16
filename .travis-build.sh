92d7cc6cb164ae85da153311046623c4264aa064
Mark Raynsford <code@io7m.com>
2020-04-16 10:47:26 +0000

    Document the org.gradle.internal.publish.checksums.insecure property, and update Travis CI builds

--- .travis-build.sh
+++ .travis-build.sh
@@ -4,7 +4,9 @@
 # Execute the build
 
 ./gradlew clean test assemble \
-  -Porg.librarysimplified.keyAlias=nypl \
-  -Porg.librarysimplified.keyPassword=${NYPL_SIGNING_KEY_PASSWORD} \
-  -Porg.librarysimplified.storePassword=${NYPL_SIGNING_STORE_PASSWORD} \
+  -Dorg.gradle.internal.publish.checksums.insecure=true                 \
+  -Porg.librarysimplified.keyAlias=nypl                                 \
+  -Porg.librarysimplified.keyPassword=${NYPL_SIGNING_KEY_PASSWORD}      \
+  -Porg.librarysimplified.storePassword=${NYPL_SIGNING_STORE_PASSWORD}  \
   -Porg.gradle.parallel=false
+
