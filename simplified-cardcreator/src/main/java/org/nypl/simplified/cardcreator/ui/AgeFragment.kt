0d9f4ad7b558667e8ce497b251ae5bf2ae936c99
malcolmmcfly <mjw0133@gmail.com>
2020-04-27 10:07:12 -0500

    Removed unused import

--- simplified-cardcreator/src/main/java/org/nypl/simplified/cardcreator/ui/AgeFragment.kt
+++ simplified-cardcreator/src/main/java/org/nypl/simplified/cardcreator/ui/AgeFragment.kt
@@ -1,7 +1,6 @@
 package org.nypl.simplified.cardcreator.ui
 
 import android.app.Activity
-import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
