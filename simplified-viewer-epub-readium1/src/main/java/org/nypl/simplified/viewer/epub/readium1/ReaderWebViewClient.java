e3c572060c67a6c1a2f3e8a0d8817f6d38057626
Mark Raynsford <code@io7m.com>
2021-01-20 12:34:18 +0000

    Intercept calls to non-local URIs
    This adds code to reject any calls made to non-localhost URIs in
    Readium 1. This is desired by LFA, who want to prevent the limited
    bandwidth available to classrooms being wasted by accidental redirects
    to real servers on the internet from inside EPUB files.

--- simplified-viewer-epub-readium1/src/main/java/org/nypl/simplified/viewer/epub/readium1/ReaderWebViewClient.java
+++ simplified-viewer-epub-readium1/src/main/java/org/nypl/simplified/viewer/epub/readium1/ReaderWebViewClient.java
@@ -11,31 +11,33 @@ import com.io7m.jnull.Nullable;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.io.ByteArrayInputStream;
 import java.io.InputStream;
 import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.HashMap;
+import java.util.Objects;
 
 /**
  * The web client responsible for overriding the requests for certain
  * resources.
  */
 
-final class ReaderWebViewClient extends WebViewClient
-{
+final class ReaderWebViewClient extends WebViewClient {
   private static final Logger LOG = LoggerFactory.getLogger(ReaderWebViewClient.class);
 
   private final ReaderSimplifiedFeedbackDispatcherType simplified_dispatcher;
-  private final ReaderReadiumFeedbackDispatcherType    readium_dispatcher;
-  private final Activity                               activity;
-  private final ReaderSimplifiedFeedbackListenerType   simplified_listener;
-  private final ReaderReadiumFeedbackListenerType      readium_listener;
+  private final ReaderReadiumFeedbackDispatcherType readium_dispatcher;
+  private final Activity activity;
+  private final ReaderSimplifiedFeedbackListenerType simplified_listener;
+  private final ReaderReadiumFeedbackListenerType readium_listener;
 
   public ReaderWebViewClient(
     final Activity in_activity,
     final ReaderSimplifiedFeedbackDispatcherType in_simplified_dispatcher,
     final ReaderSimplifiedFeedbackListenerType in_simplified_listener,
     final ReaderReadiumFeedbackDispatcherType in_readium_dispatcher,
-    final ReaderReadiumFeedbackListenerType in_readium_listener)
-  {
+    final ReaderReadiumFeedbackListenerType in_readium_listener) {
     this.activity = NullCheck.notNull(in_activity);
     this.simplified_dispatcher = NullCheck.notNull(in_simplified_dispatcher);
     this.simplified_listener = NullCheck.notNull(in_simplified_listener);
@@ -43,9 +45,9 @@ final class ReaderWebViewClient extends WebViewClient
     this.readium_listener = NullCheck.notNull(in_readium_listener);
   }
 
-  private static @Nullable WebResourceResponse getInterceptedRequestResource(
-    final String url)
-  {
+  private static @Nullable
+  WebResourceResponse getInterceptedRequestResource(
+    final String url) {
     if ("simplified-resource:OpenDyslexic3-Regular.ttf".equals(url)) {
       ReaderWebViewClient.LOG.debug("intercepted {} request", url);
       final InputStream stream =
@@ -60,17 +62,17 @@ final class ReaderWebViewClient extends WebViewClient
     return null;
   }
 
-  @Override public void onLoadResource(
+  @Override
+  public void onLoadResource(
     final @Nullable WebView view,
-    final @Nullable String url)
-  {
+    final @Nullable String url) {
     ReaderWebViewClient.LOG.debug("web-request: {}", url);
   }
 
-  @Override public boolean shouldOverrideUrlLoading(
+  @Override
+  public boolean shouldOverrideUrlLoading(
     final @Nullable WebView view,
-    final @Nullable String url)
-  {
+    final @Nullable String url) {
     final String nu = NullCheck.notNull(url);
     final URI uu = NullCheck.notNull(URI.create(nu));
 
@@ -86,19 +88,46 @@ final class ReaderWebViewClient extends WebViewClient
       return true;
     }
 
+    if (!isLocalhost(uu.getHost())) {
+      LOG.debug("rejecting request to non-localhost URI");
+      return true;
+    }
+
     return super.shouldOverrideUrlLoading(view, url);
   }
 
-  @Override public WebResourceResponse shouldInterceptRequest(
+  private static boolean isLocalhost(String host) {
+    return Objects.equals(host, "127.0.0.1")
+      || Objects.equals(host, "::1")
+      || Objects.equals(host, "localhost");
+  }
+
+  @Override
+  public WebResourceResponse shouldInterceptRequest(
     final WebView view,
-    final String url)
-  {
+    final String url) {
     if (url.startsWith("simplified-resource:")) {
       final WebResourceResponse r =
         ReaderWebViewClient.getInterceptedRequestResource(url);
       if (r != null) {
         return r;
       }
+    }
+
+    try {
+      if (!isLocalhost(new URI(url).getHost())) {
+        LOG.debug("rejecting request to non-localhost URI");
+        return new WebResourceResponse(
+          "text/plain",
+          "UTF-8",
+          403,
+          "FORBIDDEN",
+          new HashMap<>(),
+          new ByteArrayInputStream(new byte[0])
+        );
+      }
+    } catch (URISyntaxException e) {
+      LOG.error("invalid URI: ", e);
     }
 
     return super.shouldInterceptRequest(view, url);
