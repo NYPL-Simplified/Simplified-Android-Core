74c407c114c6473c432986dbd836c69df088bee8
Mark Raynsford <code@io7m.com>
2021-02-18 17:00:31 +0000

    Add remaining accessibility events
    This adds events for all of the remaining book state changes that we
    want announced.
    
    Affects: https://jira.nypl.org/browse/SIMPLY-1429
    Affects: https://jira.nypl.org/browse/SIMPLY-1439
    Affects: https://jira.nypl.org/browse/SIMPLY-1440
    Affects: https://jira.nypl.org/browse/SIMPLY-1506

--- simplified-accessibility/src/main/java/org/nypl/simplified/accessibility/AccessibilityStringsType.kt
+++ simplified-accessibility/src/main/java/org/nypl/simplified/accessibility/AccessibilityStringsType.kt
@@ -7,4 +7,9 @@ package org.nypl.simplified.accessibility
 interface AccessibilityStringsType {
   fun bookHasDownloaded(title: String): String
   fun bookIsDownloading(title: String): String
+  fun bookIsOnHold(title: String): String
+  fun bookReturned(title: String): String
+  fun bookFailedReturn(title: String): String
+  fun bookFailedLoan(title: String): String
+  fun bookFailedDownload(title: String): String
 }
