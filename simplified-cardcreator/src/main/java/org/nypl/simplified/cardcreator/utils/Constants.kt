631755765f6c7ac13a28c314daba1cc2287e6dbd
Malcolm Woods <mjw0133@gmail.com>
2022-05-19 10:58:09 -0500

    Remove unused card creator base URLs (#983)

--- simplified-cardcreator/src/main/java/org/nypl/simplified/cardcreator/utils/Constants.kt
+++ simplified-cardcreator/src/main/java/org/nypl/simplified/cardcreator/utils/Constants.kt
@@ -4,9 +4,6 @@ package org.nypl.simplified.cardcreator.utils
  * Base URLs used with Library Simplified endpoints
  */
 object Constants {
-  const val LIBRARY_SIMPLIFIED_BASE_URL = "http://patrons.librarysimplified.org/"
-  const val LIBRARY_SIMPLIFIED_QA_BASE_URL = "http://qa.patrons.librarysimplified.org/"
-
   const val EULA = "https://librarysimplified.org/EULA/"
   const val LEGAL_DISCLAIMER = "https://www.nypl.org/help/library-card/terms-conditions"
 
